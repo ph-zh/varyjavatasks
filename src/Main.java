571fb3c53324ec4db1d7928bfbca3f5ea080b2d3
phzh <p.zhavoronkov@gmail.com>
2025-01-16 21:21:07 +0100

    temperature conversion calculator in java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1\t2\t3\n4\t5\t6\n7\t8\t9");
+        Scanner scanner = new Scanner(System.in);
+
+        String unit;
+        float temp;
+
+        System.out.println("Is the temperature in (F) or (C)? ");
+        unit = scanner.nextLine();
+
+        unit = unit.toUpperCase();
+
+        if(unit.equals("C")) {
+            System.out.println("Enter temperature in Celcius: ");
+            temp = scanner.nextFloat();
+            temp = temp * 9 / 5 + 32;
+            System.out.println("The temp in Fahrenheit is: " + temp);
+
+        } else if (unit.equals("F")) {
+            System.out.println("Enter temperature in Fahrenheit: ");
+            temp = scanner.nextFloat();
+            temp = (temp - 32) * 5 / 9;
+            System.out.println("The temp in Celcius is: " + temp);
+
+        } else {
+            System.out.println(unit + " is not a valid unit of measurement");
+        }
+
     }
 }
