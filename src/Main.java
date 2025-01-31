dbdc3bf88f82310e8f1e11c1df35a6d3f9407b66
phzh <p.zhavoronkov@gmail.com>
2025-01-31 21:29:50 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,11 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        String unit;
-        float temp;
-
-        System.out.println("Is the temperature in (F) or (C)? ");
-        unit = scanner.nextLine();
-
-        unit = unit.toUpperCase();
-
-        if(unit.equals("C")) {
-            System.out.println("Enter temperature in Celcius: ");
-            temp = scanner.nextFloat();
-            temp = temp * 9 / 5 + 32;
-            System.out.println("The temp in Fahrenheit is: " + temp);
-
-        } else if (unit.equals("F")) {
-            System.out.println("Enter temperature in Fahrenheit: ");
-            temp = scanner.nextFloat();
-            temp = (temp - 32) * 5 / 9;
-            System.out.println("The temp in Celcius is: " + temp);
-
-        } else {
-            System.out.println(unit + " is not a valid unit of measurement");
-        }
-
+        String[] cars = {"Toyota", "BMW", "KIA"};
+        cars[0] = "Mustang";
+            for (int i = 0; i < cars.length; i++) {
+                System.out.println(i + 1 + ". " + cars[i]);
+            }
+        System.out.println("Number of cars in the pool: " + cars.length);
     }
 }
