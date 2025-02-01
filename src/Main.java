cb2b92d406b153e2988b79608061d869852b3491
phzh <p.zhavoronkov@gmail.com>
2025-02-01 21:42:32 +0100

    update2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        String[] cars = {"Toyota", "BMW", "KIA"};
+        String[] cars = {"Toyota", "Mercedes", "KIA"};
         cars[0] = "Mustang";
             for (int i = 0; i < cars.length; i++) {
                 System.out.println(i + 1 + ". " + cars[i]);
