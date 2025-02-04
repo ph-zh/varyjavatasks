0586cca015c3f91077fd08ab60973c3c617e1767
phzh <p.zhavoronkov@gmail.com>
2025-02-04 23:48:31 +0100

    enum

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        String[] cars = {"Toyota", "Mercedes", "KIA"};
-        cars[0] = "Mustang";
-            for (int i = 0; i < cars.length; i++) {
-                System.out.println(i + 1 + ". " + cars[i]);
-            }
-        System.out.println("Number of cars in the pool: " + cars.length);
+        enum Day;
+
     }
 }
