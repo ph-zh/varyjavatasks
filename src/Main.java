e15088332f6af0cd67b07039a41db0086e40fec6
phzh <p.zhavoronkov@gmail.com>
2025-02-10 23:26:17 +0100

    update3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test2");
+        System.out.println("test3");
+        System.out.println("Hello");
     }
 }
