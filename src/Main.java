edcc24abbf242ebc5a6907af9fa69727b44bb5a8
phzh <p.zhavoronkov@gmail.com>
2025-03-29 15:15:22 +0100

    double

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Это напечатано на одной строке.");
-        System.out.println("А это на другой!");
+        double pi = 3.14;
+        System.out.println(pi);
     }
 }
