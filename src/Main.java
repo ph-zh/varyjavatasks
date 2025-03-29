868838d439a296ff73b8359b37bdb6402ea060ef
phzh <p.zhavoronkov@gmail.com>
2025-03-29 19:41:37 +0100

    minutesperday

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        double pi = 3.14;
-        System.out.println(pi);
+        int minutesPerHour = 60;
+        int miutesPerDay = minutesPerHour * 24;
+        System.out.println(miutesPerDay);
     }
 }
