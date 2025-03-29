d9eafa70199f2cbd5a816c146b4b0f33f09cee82
phzh <p.zhavoronkov@gmail.com>
2025-03-29 12:57:04 +0100

    println

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("test2");
+        System.out.println("Это напечатано на одной строке.");
+        System.out.println("А это на другой!");
     }
 }
