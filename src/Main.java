8140dbbbf31e07ff7b0831d9b481ae04308149f6
phzh <p.zhavoronkov@gmail.com>
2025-04-06 20:49:42 +0200

    quick reminder

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        double price = 120.5; // Цена за килограмм
-        double weight = 3.5; // Вес в килограммах
-        System.out.println("Цена за " + weight + " килограмма = " + price * weight);
+        int a; // Тип для целых чисел (0, 6, 10, -23 и т. д.)
+        double b; // Тип для дробных чисел (3.5, 6.4 и т. д.)
+        String c; // Тип для строк ("Привет")
+
+        double pi = 3.14; // Создаёт переменную и присваивает ей значение
+        int solution = 25 / 5; // Делит 25 на 5
+
+        String newString = "Привет," + " " + "мир"; // Конкатенация строк
+        String helloJava = "Я изучаю Java"; // Переменная со строкой
+        System.out.println(helloJava); // Напечатает "Я изучаю Java"
+        System.out.println(newString); // Напечатает "Привет, мир"
     }
 }
