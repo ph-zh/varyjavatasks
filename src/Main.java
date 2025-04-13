8eae6456272e05308e46051bfe57bcce6226e039
phzh <p.zhavoronkov@gmail.com>
2025-04-13 21:07:34 +0200

    calculator task

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,22 @@
 /*
-Исправьте проблему с областью видимости переменной rateUSD.
-Объявите её после переменной condition и там же инициализируйте — присвойте значение  78.8.
-true
+Давайте ещё немного прокачаем приложение. Сделаем так, чтобы оно могло конвертировать не только в
+доллары, но и в евро. В переменной currency сохраните название валюты: USD или EUR.
+В зависимости от того, какая валюта хранится в переменной, пользователь увидит сумму своих
+сбережений в долларах или евро.
  */
 class Main {
     public static void main(String[] args) {
-        boolean condition = true;
-        // Перенесите объявление переменной сюда
-        double rateUSD = 78.8;
+        double rateUSD = 78.5;
+        double rateEUR = 85.5;
+        double rubles = 14560.5;
 
-        if (condition) {
-            System.out.println("Внутри блока курс доллара виден. Он равен " + rateUSD);
+        String currency = "USD";
+        System.out.println("Вы конвертируете рубли в " + currency);
+        // Если currency равно "USD", то конвертируем в доллары, иначе в евро
+        if(currency.equals("USD")) {
+            System.out.println("Ваши сбережения в долларах: " + rubles / rateUSD);
+        } else {
+            System.out.println("Ваши сбережения в евро: " + rubles / rateEUR);
         }
-        // Здесь переменная rateUSD не видна
-        System.out.println("Вне блока курс доллара не виден. Должен быть " + rateUSD);
     }
 }
