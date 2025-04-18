695260cb976a78df1c22386e21d9cb7e7e3af8d4
phzh <p.zhavoronkov@gmail.com>
2025-04-18 18:33:45 +0200

    if else task 2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,27 @@
 /*
-Измените остаток средств на счету так, чтобы приложение порекомендовало поужинать в Макдоналдсе.
+Как мы ни старались, но в код прокралась ошибка! Вам предстоит её исправить:
+конвертация в иены работает, но проблема возникает, если попытаться конвертировать в новую валюту,
+например, датскую крону — DKK. Программа в таком случае конвертирует в иены, а хотелось бы получить
+сообщение об ошибке, ведь приложение пока не поддерживает такую валюту.
  */
 class Main {
     public static void main(String[] args) {
-        double moneyBeforeSalary = 9000.0; // Количество денег до зарплаты
+        double rubles = 14560.5;
+        double rateUSD = 78.5;
+        double rateEUR = 85.5;
+        double rateJPY = 0.74; // Курс японской иены
 
-        if (moneyBeforeSalary < 3000) {
-            System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
-        } else if (moneyBeforeSalary < 10000) {
-            System.out.println("Окей, пора в Макдак!");
+        String currency = "DKK";
+        System.out.println("Вы конвертируете рубли в " + currency);
+
+        if (currency.equals("USD")) {
+            System.out.println("Ваши сбережения в долларах: " + rubles / rateUSD);
+        } else if (currency.equals("EUR")) {
+            System.out.println("Ваши сбережения в евро: " + rubles / rateEUR);
+        } else if (currency.equals("JPY")) {
+            System.out.println("Ваши сбережения в иенах: " + rubles / rateJPY);
         } else {
-            System.out.println("Класс! Заказывайте крабов!");
+            System.out.println("Валюта не поддерживается.");
         }
     }
 }
