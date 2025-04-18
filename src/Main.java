d0116964f4647e97d194a29e9b5baca08d121d07
phzh <p.zhavoronkov@gmail.com>
2025-04-18 18:18:36 +0200

    if else task

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,16 @@
 /*
-Давайте ещё немного прокачаем приложение. Сделаем так, чтобы оно могло конвертировать не только в
-доллары, но и в евро. В переменной currency сохраните название валюты: USD или EUR.
-В зависимости от того, какая валюта хранится в переменной, пользователь увидит сумму своих
-сбережений в долларах или евро.
+Измените остаток средств на счету так, чтобы приложение порекомендовало поужинать в Макдоналдсе.
  */
 class Main {
     public static void main(String[] args) {
-        double rateUSD = 78.5;
-        double rateEUR = 85.5;
-        double rubles = 14560.5;
+        double moneyBeforeSalary = 9000.0; // Количество денег до зарплаты
 
-        String currency = "USD";
-        System.out.println("Вы конвертируете рубли в " + currency);
-        // Если currency равно "USD", то конвертируем в доллары, иначе в евро
-        if(currency.equals("USD")) {
-            System.out.println("Ваши сбережения в долларах: " + rubles / rateUSD);
+        if (moneyBeforeSalary < 3000) {
+            System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
+        } else if (moneyBeforeSalary < 10000) {
+            System.out.println("Окей, пора в Макдак!");
         } else {
-            System.out.println("Ваши сбережения в евро: " + rubles / rateEUR);
+            System.out.println("Класс! Заказывайте крабов!");
         }
     }
 }
