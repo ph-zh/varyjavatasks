e9e1025521222a00d6daa8626f72bbf5d08b7d1f
phzh <p.zhavoronkov@gmail.com>
2025-04-20 17:29:59 +0200

    loop task

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,12 @@
-import java.util.Scanner;
-
+/*
+Циклы — сложная тема. Новой информации оказалось так много, что у вас никак не получается заснуть. Требуется применить проверенный метод — посчитать овечек.
+Напишите цикл, в котором они одна за другой будут прыгать через забор. Вам нужно пересчитать 17 овечек. Тогда вы сможете отдохнуть с чистой совестью.
+ */
 class Main {
     public static void main(String[] args) {
-        double rateUSD = 78;
-        double rateEUR = 85;
-        double rateJPY = 0.74;
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Сколько денег у вас осталось до зарплаты?");
-        double moneyBeforeSalary = scanner.nextDouble();
-
-        System.out.println("Сколько дней до зарплаты?");
-        int daysBeforeSalary = scanner.nextInt();
-
-        System.out.println("Введите команду. Доступные команды: convert и advice.");
-        String command = scanner.next();
-
-        if (command.equals("convert")) {
-
-            System.out.println("В какую валюту хотите конвертировать рубли? Доступные варианты: USD, EUR, JPY.");
-            String currency = scanner.nextLine(); // Считайте это значение с помощью scanner
-            if (currency.equals("USD")) {
-                System.out.println("Ваши сбережения в долларах: " + moneyBeforeSalary / rateUSD);
-            } else if (currency.equals("EUR")) {
-                System.out.println("Ваши сбережения в евро: " + moneyBeforeSalary / rateEUR);
-            } else if (currency.equals("JPY")) {
-                System.out.println("Ваши сбережения в иенах: " + moneyBeforeSalary / rateJPY);
-            } else {
-                System.out.println("Валюта не поддерживается.");
-            }
-
-        } else if (command.equals("advice")) {
-            if (moneyBeforeSalary < 3000) {
-                System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
-            } else if (moneyBeforeSalary < 10000) {
-                if (daysBeforeSalary < 10) {
-                    System.out.println("Окей, пора в Макдак!");
-                } else {
-                    System.out.println("Сегодня лучше поесть дома. Экономьте, и вы дотянете до зарплаты!");
-                }
-            } else if (moneyBeforeSalary < 30000) {
-                if (daysBeforeSalary < 10) {
-                    System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место. :)");
-                } else {
-                    System.out.println("Окей, пора в Макдак!");
-                }
-            } else {
-                if (daysBeforeSalary < 10) {
-                    System.out.println("Отлично! Заказывайте крабов!");
-                } else {
-                    System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место. :)");
-                }
-            }
-        } else {
-            System.out.println("Извините, такой команды пока нет.");
+        // Вместо точек подставьте нужные выражения
+        for (int i = -3; i <= 11; i = i + 1) {
+            System.out.println("Этаж " + i);
         }
     }
 }
