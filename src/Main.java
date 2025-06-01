ee9060eb516caf1fca83586a7a37ea0e1ed1fb21
phzh <p.zhavoronkov@gmail.com>
2025-06-01 19:51:32 +0200

    parameter & argument example

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,22 @@
 /*
-Перед вами код приложения, которое умеет давать пользователю советы о покупке долларов и евро в зависимости от их курса.
-Однако сейчас код не запускается — в нём допущены ошибки. Вам необходимо исправить их. Учтите, что после выполнения команд с выводом советов программа должна ещё раз запрашивать у пользователя ввод команды.
-При выборе команды «Выход» приложение должно завершить работу.
+
 */
-import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Привет!");
-        doCommand();
+        int result = add(2, 3); // Вызываем метод с аргументами 2 и 3, сохраняем результат в result
+        System.out.println("Тут всё просто, 2 + 3 = " + result); // Печатаем пример
+
+        // Второй раз вызываем метод
+        result = add(338 + 665, 551 + 779); // В качестве аргументов переданы суммы чисел
+        System.out.println("Считали-считали, и получилось " + result);
+
+        // Посчитаем то же самое, но с помощью вызова метода
+        result = add(add(338, 665), add(551, 779)); // Аргументы — возвращённые значения метода
+        System.out.println("Ничего не изменилось, здесь получается " + result);
     }
 
-    public static void doCommand() {
-        Scanner scanner = new Scanner(System.in);
-
-
-        while (true) {
-            System.out.println("Что вы хотите сделать?");
-            System.out.println("1 - Получить совет о покупке евро");
-            System.out.println("2 - Получить совет о покупке долларов");
-            System.out.println("0 - Выход");
-            int command = scanner.nextInt();
-            if (command == 1) {
-                System.out.println("Если евро стоит меньше 90 рублей - покупайте!");
-            } else if (command == 2) {
-                System.out.println("Если доллар стоит меньше 75 рублей - покупайте!");
-            } else if (command == 0) {
-                System.out.println("Выход");
-                return;
-            }
-        }
+    public static int add(int a, int b) { // Метод принимает два параметра — два целых числа
+        return a + b;
     }
 }
