a9dae83d9e6e93cdc20009ca2c8ee2d4f6622515
phzh <p.zhavoronkov@gmail.com>
2025-06-03 23:07:42 +0200

    value update

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@
 
 class Main {
     public static void main(String[] args) {
-        double[] expenses = {1772.5, 367.0, 120.6, 2150.2, 874.0, 1.0, 1459.4};
+        double[] expenses = {1772.0, 367.0, 120.6, 2150.2, 874.0, 1.0, 1459.4};
         double maxExpense = findMaxExpense(expenses); // Вызовите метод и присвойте maxExpense значение его результата
         System.out.println("Самая большая трата недели " + maxExpense);
     }
