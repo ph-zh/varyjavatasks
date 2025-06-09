62d354a6de238ce5386b32d136d2f91259fbe909
phzh <p.zhavoronkov@gmail.com>
2025-06-09 15:07:37 +0200

    hamseter weight task

---
+++ src/Hamster.java
@@ -0,0 +1,25 @@
+public class Hamster {
+
+    //Поля
+    String name = "Байт"; // Имя
+    int age = 2; // Возраст
+    String color = "Рыжий"; // Цвет
+    int weight = 350;
+
+    // Методы
+    void eat(int foodWeight) {
+        weight = weight + foodWeight;
+    }
+
+    // Бегать в колесе
+    void runInWheel() {
+        System.out.println("Бегу-бегу-бегу!");
+        weight = weight - 5;
+    }
+
+    // Прятать семечки
+    void hideSeeds(int seedWeight) {
+        System.out.println("Зимой не заголодаю.");
+        weight = weight + seedWeight;
+    }
+}
