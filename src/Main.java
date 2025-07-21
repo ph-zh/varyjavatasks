ac6eb5defb58e5414a8069bb10daa9cd9c6d6916
phzh <p.zhavoronkov@gmail.com>
2025-07-21 17:43:18 +0200

    findMax method

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,20 @@
 /*
-Вам нужно дописать реализацию метода findHighestGrossingFilm(), который выбирает самый кассовый фильм из трёх предложенных вариантов —  хитов
-Джеймса Кэмерона «Титаник» и «Аватар» и блокбастера Кристофера Нолана «Тёмный рыцарь».
-Названия фильмов и их сборы заранее сохранены в соответствующих переменных.
-Метод должен сравнить суммы сборов и вернуть значение — название фильма, заработавшего в прокате больше всего.
+Исправьте код так, чтобы метод сработал и вернул большее из двух чисел.
  */
 public class Main {
-
     public static void main(String[] args) {
-        // Ниже вызовите новый метод
-        String highestGrossingFilm = findHighestGrossingFilm();
-        System.out.println("Самый кассовый фильм: " + highestGrossingFilm);
+        System.out.println("Наибольшее из чисел = " + findMax());
     }
 
-    public static String findHighestGrossingFilm() {
-        String film1 = "Титаник";
-        int income1 = 2194;
-
-        String film2 = "Аватар";
-        int income2 = 2810;
-
-        String film3 = "Тёмный рыцарь";
-        int income3 = 1084;
-
-        // Допишите реализацию метода ниже
-        if (income1 > income2) {
-            if (income1 > income3) {
-                return film1;
-            }
-        } else if (income2 > income1) {
-            if (income2 > income3) {
-                return film2;
-            }
-        } else if (income3 > income1) {
-            if (income3 > income2) {
-                return film3;
-            }
+    public static int findMax() {
+        int a = 10;
+        int b = 15;
+        if (a > b) {
+            System.out.println("а больше b");
+            return a;
+        } else {
+            System.out.println("b больше a");
+            return b;
         }
-        return film3;
     }
 }
