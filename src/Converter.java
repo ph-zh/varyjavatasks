cb8497e1063f77ed9e4970352cabc95cf1827dbc
phzh <p.zhavoronkov@gmail.com>
2025-07-24 22:16:24 +0200

    class converter and it method declartion in main

--- src/Converter.java
+++ src/Converter.java
@@ -1,13 +1,8 @@
-/*
-Вам нужно, чтобы конвертер умел рассчитать соотношение валют. Для этого объявите метод: convert. Этот метод должен принимать такие параметры, как сумма в рублях (double rubles)
-и код валюты (int currency). Напишите определение метода и поместите в него логику конвертации валюты.
- */
 public class Converter {
     double rateUSD = 78.5;
     double rateEUR = 88.7;
     double rateJPY = 0.75;
 
-    // Тело метода
     void convert(double rubles, int currency) {
         if (currency == 1) {
             System.out.println("Ваши сбережения в долларах: " + rubles / rateUSD);
