c9ff922e29742aeb75be5b17ff918f65163be09a
phzh <p.zhavoronkov@gmail.com>
2025-07-25 11:28:52 +0200

    constructor task example

--- src/Hamster.java
+++ src/Hamster.java
@@ -1,24 +1,14 @@
 public class Hamster {
-    //Поля
-    String name = "Байт"; // Имя
-    int age = 2; // Возраст
-    String color = "Рыжий"; // Цвет
-    int weight = 350;
 
-    // Методы
-    void eat(int foodWeight) {
-        weight = weight + foodWeight;
-    }
+    String name;  // Имя
+    int age;      // Возраст
+    int weight;   // Вес в граммах
+    String color; // Цвет
 
-    // Бегать в колесе
-    void runInWheel() {
-        System.out.println("Бегу-бегу-бегу!");
-        weight = weight - 5;
-    }
-
-    // Прятать семечки
-    void hideSeeds(int seedWeight) {
-        System.out.println("Зимой не заголодаю.");
-        weight = weight + seedWeight;
+    Hamster(String hamsterName, int hamsterAge, int hamsterWeight, String hamsterColor) {
+        name = hamsterName;
+        age = hamsterAge;
+        weight = hamsterWeight;
+        color = hamsterColor;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,16 @@
 /*
-
+Из-за того что мы добавили в класс Hamster конструктор с параметрами, код из предыдущего урока перестал работать. Перепишите его.
  */
 public class Main {
     public static void main(String[] args) {
-        Hamster bite = new Hamster(); // Имя Байта менять не нужно
-        bite.eat(35); // 370
-        for (int i = 0; i < 3; i++) {
-            bite.runInWheel();
-        }
-        System.out.println("Bite weight" + bite.weight);
-
-        Hamster ninja = new Hamster();
-        ninja.name = "Ниндзя";
-        ninja.weight = 250;
-        ninja.runInWheel(); // 290
-        if (ninja.weight < 265) {
-            ninja.eat(45);
-        }
-        System.out.println("Ninja weight" + ninja.weight);
-
-        Hamster kuzya = new Hamster();
-        kuzya.name = "Кузя";
-        kuzya.weight = 450;
-        kuzya.weight = 325;
-        kuzya.eat(20); // 345
-        System.out.println("Kuzya weight" + kuzya.weight);
-
-        Hamster paskal = new Hamster();
-        paskal.name = "Паскаль";
-        paskal.weight = 320; // 320
-        System.out.println("Paskal weight" + paskal.weight);
+        Hamster bite = new Hamster("Байт",2,350, "Рыжий");
+        Hamster ninja = new Hamster("Ниндзя",1,250, "Белый");
+        Hamster kuzya = new Hamster("Кузя",3, 450, "Пятнистый");
+        Hamster paskal = new Hamster("Паскаль", 2, 320, "Чёрный");
+        
+        System.out.println(bite.name);
+        System.out.println(ninja.name);
+        System.out.println(kuzya.name);
+        System.out.println(paskal.name);
     }
 }
