e8deb90d4c7c12e76cf43d1a5fb80bc62702d65d
phzh <p.zhavoronkov@gmail.com>
2025-07-29 22:08:40 +0200

    parsing task

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,14 @@
 /*
-Прочитайте код приложения онлайн-банка, где пользователь может по желанию завести счета в трёх валютах.
-В программу забрался баг — вне зависимости от выбора пользователя сразу открываются все три счёта.
-С помощью классов-обёрток и их свойств исправьте код так, чтобы открывались счета только в тех валютах,
-которые выберет пользователь. Счета необходимо создавать с нулевым балансом.
-*/
-import java.util.Scanner;
 
+*/
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите имя пользователя:");
-        String name = scanner.nextLine();
-        PersonAccount user = new PersonAccount(name);
+        String firstNumber = "123.45";
+        String secondNumber = "234.56";
+        System.out.println(addNumbers(firstNumber, secondNumber));
+    }
 
-        System.out.println("Хотите открыть счёт в RUB?");
-        System.out.println("1 - Да");
-        System.out.println("2 - Нет");
-        int command = scanner.nextInt();
-        if (command == 1) {
-            user.moneyRUB = 0.0;
-        }
-
-        System.out.println("Хотите открыть счёт в USD?");
-        System.out.println("1 - Да");
-        System.out.println("2 - Нет");
-        command = scanner.nextInt();
-        if (command == 1) {
-            user.moneyUSD = 0.0;
-        }
-
-        System.out.println("Хотите открыть счёт в EUR?");
-        System.out.println("1 - Да");
-        System.out.println("2 - Нет");
-        command = scanner.nextInt();
-        if (command == 1) {
-            user.moneyEUR = 0.0;
-        }
-
-        System.out.println("Поздравляем, аккаунт для пользователя " + user.name + " создан.");
-        System.out.println("Открытые счета:");
-        if (user.moneyRUB != null) {
-            System.out.println("- RUB");
-        }
-        if (user.moneyUSD != null) {
-            System.out.println("- USD");
-        }
-        if (user.moneyEUR != null) {
-            System.out.println("- EUR");
-        }
+    private static Float addNumbers(String firstNumber, String secondNumber) {
+        return (Float.parseFloat(firstNumber) + Float.parseFloat(secondNumber));
     }
 }
