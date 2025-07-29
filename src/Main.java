0dcf76c153db4faa6aeaa01a717b121dfe338b80
ph.zh <p.zhavoronkov@gmail.com>
2025-07-29 13:17:38 +0200

    wrapper classes task example

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,51 @@
 /*
-
+Прочитайте код приложения онлайн-банка, где пользователь может по желанию завести счета в трёх валютах.
+В программу забрался баг — вне зависимости от выбора пользователя сразу открываются все три счёта.
+С помощью классов-обёрток и их свойств исправьте код так, чтобы открывались счета только в тех валютах,
+которые выберет пользователь. Счета необходимо создавать с нулевым балансом.
 */
 public class Main {
     public static void main(String[] args) {
-        int a = 15;
-        int b = 3;
-        int c = -7;
-        Integer[] numbers = new Integer[3];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя пользователя:");
+        String name = scanner.nextLine();
+        PersonAccount user = new PersonAccount(name);
 
-        // Сохраните в массив переменные a, b и с
-        numbers[0] = a;
-        numbers[1] = b;
-        numbers[2] = c;
-        // Напечатайте все элементы
-        System.out.println("Сейчас в массиве числа: " + numbers[0] + ","
-                + numbers[1] + "," + numbers[2]);
+        System.out.println("Хотите открыть счёт в RUB?");
+        System.out.println("1 - Да");
+        System.out.println("2 - Нет");
+        int command = scanner.nextInt();
+        if (command == 1) {
+            user.moneyRUB = 0.0;
+        }
+
+        System.out.println("Хотите открыть счёт в USD?");
+        System.out.println("1 - Да");
+        System.out.println("2 - Нет");
+        command = scanner.nextInt();
+        if (command == 1) {
+            user.moneyUSD = 0.0;
+        }
+
+        System.out.println("Хотите открыть счёт в EUR?");
+        System.out.println("1 - Да");
+        System.out.println("2 - Нет");
+        command = scanner.nextInt();
+        if (command == 1) {
+            user.moneyEUR = 0.0;
+        }
+
+        System.out.println("Поздравляем, аккаунт для пользователя " + user.name + " создан.");
+        System.out.println("Открытые счета:");
+        if (user.moneyRUB == 0.0) {
+            System.out.println("- RUB");
+        }
+        if (user.moneyUSD == 0.0) {
+            System.out.println("- USD");
+        }
+        if (user.moneyEUR == 0.0) {
+            System.out.println("- EUR");
+        }
+
     }
 }
