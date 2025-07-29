cc5b622174d0e8be93ece3282a7e50694a7fdfac
ph.zh <p.zhavoronkov@gmail.com>
2025-07-29 11:26:31 +0200

    boxing task example

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,19 @@
 /*
-Часто при обновлении программ необходимо поддерживать старый код. Доработайте код новой
-версии игры-стратегии, чтобы он поддерживал параметры из старой версии:
- 1/ Поле gold типа long необходимо сохранить в переменную characterGold типа int
-(точно известно, что значение параметра gold меньше 2 000 000 000).
- 2/ Поле silver типа byte необходимо сохранить как переменную characterSilver типа int,
-предварительно умножив её на 100.
- 3/ Поле wood типа short необходимо сохранить как переменную characterWood типа double.
- 4/ Поле health типа long необходимо сохранить как переменную characterHealth типа byte
-(точно известно, что значение параметра health меньше 100).
- 5/ Поле hasHelper типа boolean необходимо сохранить как переменную characterHelpersNumber типа byte.
-Если параметр hasHelper равен true, то в переменную characterHelpersNumber сохранить единицу, если false — ноль.
+
 */
 public class Main {
     public static void main(String[] args) {
-        long inputGold = 200L;
-        byte inputSilver = 39;
-        short inputWood = 2005;
-        long inputHealth = 97L;
-        boolean inputHasHelper = true;
+        int a = 15;
+        int b = 3;
+        int c = -7;
+        Integer[] numbers = new Integer[3];
 
-        Resources characterResources = new Resources(
-                inputGold,
-                inputSilver,
-                inputWood,
-                inputHealth,
-                inputHasHelper
-        );
-
-        int characterGold = (int) characterResources.gold;
-        int characterSilver = characterResources.silver * 100;
-        double characterWood = characterResources.wood;
-        byte characterHealth = (byte) characterResources.health;
-        byte characterHelpersNumber;
-
-        // Установка значения characterHelpersNumber в зависимости от значения hasHelper
-        if(characterResources.hasHelper) {
-            characterHelpersNumber = 1;
-        } else {
-            characterHelpersNumber = 0;
-        }
-
-        Character character = new Character(
-                characterGold,
-                characterSilver,
-                characterWood,
-                characterHealth,
-                characterHelpersNumber
-        );
-
-        System.out.println("Персонаж создан успешно!");
-        System.out.println("Количество золота: " + character.gold);
-        System.out.println("Количество серебра: " + character.silver);
-        System.out.println("Количество дерева: " + character.wood);
-        System.out.println("Здоровье: " + character.health);
-        System.out.println("Количество помощников: " + character.helpersNumber);
-        System.out.println("Навстречу приключениям!");
+        // Сохраните в массив переменные a, b и с
+        numbers[0] = a;
+        numbers[1] = b;
+        numbers[2] = c;
+        // Напечатайте все элементы
+        System.out.println("Сейчас в массиве числа: " + numbers[0] + ","
+                + numbers[1] + "," + numbers[2]);
     }
 }
