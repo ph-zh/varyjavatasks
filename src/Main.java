2e384dd862d9f410222dc870eab3276985560bba
phzh <p.zhavoronkov@gmail.com>
2025-07-31 00:08:01 +0200

    parsing task template

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,37 @@
 /*
-Допишите реализацию метода, который возвращает максимум двух чисел типа byte. Необходимо использовать метод Integer.max(int, int).
+Кота Пикселя на вечер субботы оставили бабушке. Она приготовила ему на выбор говядину и курицу, а перед сном налила молока и поставила блюдце сливок.
+Пиксель внимательно следит за питанием и в обоих случаях выбрал наименее калорийную еду. Вычислите, сколько всего ккал съел Пиксель за день, и проверьте, уложился ли питомец в свой лимит в 100 ккал.
+Чтобы код сработал, вам также нужно найти и исправить ошибки, касающиеся типов.
 */
 public class Main {
     public static void main(String[] args) {
-        byte a = 10;
-        byte b = 20;
-        System.out.println(findMax(a, b));
+        String pixelKcalDay = "43"; // столько калорий съел Пиксель до похода к бабушке
+        String beefKcal = "30.2"; // калорийность говядины
+        String chickenKcal = "23.8"; // калорийность курицы
+        String creamKcal = "32.1"; // калорийность сливок
+        String milkKcal = "13.5"; // калорийность молока
+
+        float pixelChoice = getMinKcalsSum(beefKcal, chickenKcal, creamKcal, milkKcal);
+        float totalKcal = int.parseInt(pixelKcalDay) + pixelChoice;
+        checkKcal(totalKcal);
     }
 
-    private static byte findMax(byte firstNumber, byte secondNumber) {
-        return (byte) Integer.max(firstNumber, secondNumber);
+    private static Float getMinKcalsSum(String firstDishKcal, String secondDishKcal, String firstDesert, String secondDesert) {
+        float minDishKcal = ... // вычислите минимальную калорийность основного блюда
+        float minDesertKcal = ... // вычислите минимальную калорийность десерта
+        return minDishKcal + minDesertKcal;
+    }
+
+    private static void checkKcal(float catKcal) {
+        if (catKcal == null) {
+            System.out.println("Что-то пошло не так");
+        } else {
+            System.out.println("Калорийность рациона Пикселя за день: " + catKcal);
+            if (catKcal > 100) {
+                System.out.println("Пиксель сегодня не уложился в норму.");
+            } else {
+                System.out.println("Лимит не превышен!");
+            }
+        }
     }
 }
