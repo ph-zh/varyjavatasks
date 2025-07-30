91bc7c6681e3cf6a380506184f07335f8212b5af
phzh <p.zhavoronkov@gmail.com>
2025-07-30 23:54:19 +0200

    return value through parsing of byte

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,14 @@
 /*
-
+Допишите реализацию метода, который возвращает максимум двух чисел типа byte. Необходимо использовать метод Integer.max(int, int).
 */
 public class Main {
     public static void main(String[] args) {
-        byte b1 = 10;
-        byte b2 = 20;
-        printMaxOfBytes(b1, b2);
-
-        short sh1 = -5;
-        short sh2 = 5;
-        printMinOfShorts(sh1, sh2);
+        byte a = 10;
+        byte b = 20;
+        System.out.println(findMax(a, b));
     }
 
-    public static void printMaxOfBytes(byte b1, byte b2) {
-        int max_of_bytes = Integer.max((int)b1, (int)b2);
-        System.out.println(max_of_bytes);
-    }
-
-    public static void printMinOfShorts(short sh1, short sh2) {
-        int min_of_shorts = Integer.min((int)sh1, (int)sh2);
-        System.out.println(min_of_shorts);
+    private static byte findMax(byte firstNumber, byte secondNumber) {
+        return (byte) Integer.max(firstNumber, secondNumber);
     }
 }
