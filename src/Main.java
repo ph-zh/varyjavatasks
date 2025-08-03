4c699f2fad270eced90c85c16602e09dfc5e8d71
phzh <p.zhavoronkov@gmail.com>
2025-08-03 19:06:29 +0200

    even numbes check through if/else

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,21 @@
 /*
-Агрегатор авиабилетов заказал разработку приложения для планирования путешествий. Доработайте фрагмент кода, отвечающий за маршрут Москва-Париж.
-В коде допущен ряд ошибок: где-то неправильно указано условие, где-то некорректно использовано отрицание. Исправьте ошибки так, чтобы приложение работало корректно в соответствии с техническим заданием.
-ТЗ от заказчика:
-Если вводится летний месяц — рекомендуем остаться в Москве.
-Если прямые билеты в Париж дешевле билетов с пересадкой в Лондоне, то советуем лететь напрямую.
-Если билеты с пересадкой в Лондоне дешевле или равны по стоимости прямым билетам в Париж и у путешественника есть британская виза, то рекомендуем лететь через Лондон.
+Золотая классика задачек на программирование!
+Напишите программу, которая для чисел, кратных 3, печатает «Яндекс», для чисел, кратных 5 — «Практикум», а для чисел, кратных одновременно 3 и 5 — «Яндекс.Практикум».
+В других случаях программа должна печатать само число.
 */
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int monthNumber;
-
-        while (true) {
-            System.out.println("Когда планируется путешествие? Введите номер месяца от 1 до 12.");
-            monthNumber = scanner.nextInt();
-
-            if (!(monthNumber > 12)) { // Инвертируйте условие
-                break;
+        for (int i = 1; i < 21; i++) {
+            // Решение должно быть в теле цикла
+            if(i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Яндекс.Практикум");
+            } else if(i % 3 == 0) {
+                System.out.println("Яндекс");
+            } else if(i % 5 == 0) {
+                System.out.println("Практикум");
             } else {
-                System.out.println("Некорректный номер месяца. Введите ещё раз.");
+                System.out.println(i);
             }
-        }
-        String season = getSeasonByNumber(monthNumber);
-
-        /* Допишите ветвление, которое будет печатать строку:
-            System.out.println("Летом лучше остаться в Москве.");
-              и завершать выполнение программы */
-        if (season.equals("Лето")) {
-            System.out.println("Летом лучше остаться в Москве.");
-            return;
-        }
-
-
-        System.out.println("Укажите стоимость прямых билетов из Москвы в Париж:");
-        int ticketMoscowParis = scanner.nextInt();
-        System.out.println("Укажите стоимость билетов из Москвы в Париж с пересадкой в Лондоне:");
-        int ticketMoscowLondonParis = scanner.nextInt();
-
-        // В этом ветвлении что-то напутано с условиями - исправьте это
-        if (!season.equals("Лето")) { // Перенесите проверку на "Лето" в ветвление выше
-            if (ticketMoscowParis >= ticketMoscowLondonParis) {
-                System.out.println("У вас есть британская виза?");
-                System.out.println("1 - да, виза есть");
-                System.out.println("0 - визы нет");
-                int britainVisa = scanner.nextInt();
-                if (britainVisa == 1) { // Упростите это условие
-                    System.out.println("Летим через Лондон!");
-                } else {
-                    System.out.println("Летим напрямую в Париж!");
-                }
-            } else {
-                System.out.println("Летим напрямую в Париж!");
-            }
-        }
-    }
-
-    private static String getSeasonByNumber(int monthNumber) {
-        // Допишите условия ветвления в виде утверждений
-        if (monthNumber <= 2) {
-            return "Зима";
-        } else if (monthNumber <= 5) {
-            return "Весна";
-        } else if (monthNumber <= 8) {
-            return "Лето";
-        } else if (monthNumber <= 11) {
-            return "Осень";
-        } else {
-            return "Зима";
         }
     }
 }
