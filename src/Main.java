2994f5a4c1f39025bf00cea73e89f7f474d7fb01
phzh <p.zhavoronkov@gmail.com>
2025-08-03 19:31:01 +0200

    direct fly task. Updated with boolean operators

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,65 @@
 /*
-Золотая классика задачек на программирование!
-Напишите программу, которая для чисел, кратных 3, печатает «Яндекс», для чисел, кратных 5 — «Практикум», а для чисел, кратных одновременно 3 и 5 — «Яндекс.Практикум».
-В других случаях программа должна печатать само число.
+В конце прошлого урока вы написали фрагмент кода приложения для планирования путешествия из Москвы в Париж. Доработайте его с учётом новых инструкций.
+ 1/ Номер месяца должен быть в диапазоне от 1 до 12.
+ 2/ В Москве рекомендуется остаться не только в летние, но и в зимние месяцы.
+ 3/ Сравнение стоимости билетов и проверку наличия визы нужно сохранить в отдельные булевы переменные.
+ 4/ Проверить наличие дешёвых билетов и визы в ветвлении нужно одним условием.
 */
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i < 21; i++) {
-            // Решение должно быть в теле цикла
-            if(i % 3 == 0 && i % 5 == 0) {
-                System.out.println("Яндекс.Практикум");
-            } else if(i % 3 == 0) {
-                System.out.println("Яндекс");
-            } else if(i % 5 == 0) {
-                System.out.println("Практикум");
+        Scanner scanner = new Scanner(System.in);
+        int monthNumber;
+
+        while (true) {
+            System.out.println("Когда планируется путешествие? Введите номер месяца от 1 до 12.");
+            monthNumber = scanner.nextInt();
+
+            if (monthNumber >= 1 && monthNumber <= 12) { // Номер месяца должен быть в диапазоне от 1 до 12
+                break;
             } else {
-                System.out.println(i);
+                System.out.println("Некорретный номер месяца. Введите ещё раз.");
             }
+        }
+
+        String season = getSeasonByNumber(monthNumber);
+
+        if (season.equals("Лето") || season.equals("Зима")) { // Не рекомендуем лететь в Париж в летние и зимние месяцы
+            System.out.println("В это время года лучше остаться в Москве.");
+            return;
+        }
+
+        System.out.println("Укажите стоимость прямых билетов из Москвы в Париж:");
+        int ticketMoscowParis = scanner.nextInt();
+        System.out.println("Укажите стоимость билетов из Москвы в Париж с пересадкой в Лондоне:");
+        int ticketMoscowLondonParis = scanner.nextInt();
+        System.out.println("У вас есть британская виза?");
+        System.out.println("1 - да, виза есть");
+        System.out.println("0 - визы нет");
+        int britainVisa = scanner.nextInt();
+
+        boolean directTicketsCheaper = ticketMoscowParis >= ticketMoscowLondonParis; // Сравнение стоимости билетов
+        boolean hasBritainVisa = britainVisa == 1; // Проверка наличия визы
+
+        if (directTicketsCheaper && hasBritainVisa) { // Проверить в одном условии наличие дешёвых билетов и визы
+            System.out.println("Летим через Лондон!");
+        } else {
+            System.out.println("Летим через Париж!");
+        }
+    }
+
+    private static String getSeasonByNumber(int monthNumber) {
+        if (monthNumber < 3) {
+            return "Зима";
+        } else if (monthNumber < 6) {
+            return "Весна";
+        } else if (monthNumber < 9) {
+            return "Лето";
+        } else if (monthNumber < 12) {
+            return "Осень";
+        } else {
+            return "Зима";
         }
     }
 }
