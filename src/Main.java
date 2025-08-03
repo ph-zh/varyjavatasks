6fab0d2701992cb20d430b8e639b8a03e2a3c87a
phzh <p.zhavoronkov@gmail.com>
2025-08-03 20:43:57 +0200

    isLeapYear check through short form

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,27 @@
 /*
-В конце прошлого урока вы написали фрагмент кода приложения для планирования путешествия из Москвы в Париж. Доработайте его с учётом новых инструкций.
- 1/ Номер месяца должен быть в диапазоне от 1 до 12.
- 2/ В Москве рекомендуется остаться не только в летние, но и в зимние месяцы.
- 3/ Сравнение стоимости билетов и проверку наличия визы нужно сохранить в отдельные булевы переменные.
- 4/ Проверить наличие дешёвых билетов и визы в ветвлении нужно одним условием.
+Вычислите с помощью логического выражения, является ли год високосным по григорианскому календарю. Для этого нужно проверить три условия:
+ - номер года делится без остатка на 400;
+ - или
+ - номер года делится без остатка на 4;
+ - и
+ - номер года не делится без остатка на 100.
+Результат проверки сохраните в переменную isLeapYear и подставьте в ветвление, которое печатает вывод в консоли. Сообщение о том, что год не високосный должно стоять в первом блоке кода ветвления.
 */
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int monthNumber;
-
-        while (true) {
-            System.out.println("Когда планируется путешествие? Введите номер месяца от 1 до 12.");
-            monthNumber = scanner.nextInt();
-
-            if (monthNumber >= 1 && monthNumber <= 12) { // Номер месяца должен быть в диапазоне от 1 до 12
-                break;
-            } else {
-                System.out.println("Некорретный номер месяца. Введите ещё раз.");
-            }
-        }
-
-        String season = getSeasonByNumber(monthNumber);
-
-        if (season.equals("Лето") || season.equals("Зима")) { // Не рекомендуем лететь в Париж в летние и зимние месяцы
-            System.out.println("В это время года лучше остаться в Москве.");
-            return;
-        }
-
-        System.out.println("Укажите стоимость прямых билетов из Москвы в Париж:");
-        int ticketMoscowParis = scanner.nextInt();
-        System.out.println("Укажите стоимость билетов из Москвы в Париж с пересадкой в Лондоне:");
-        int ticketMoscowLondonParis = scanner.nextInt();
-        System.out.println("У вас есть британская виза?");
-        System.out.println("1 - да, виза есть");
-        System.out.println("0 - визы нет");
-        int britainVisa = scanner.nextInt();
-
-        boolean directTicketsCheaper = ticketMoscowParis >= ticketMoscowLondonParis; // Сравнение стоимости билетов
-        boolean hasBritainVisa = britainVisa == 1; // Проверка наличия визы
-
-        if (directTicketsCheaper && hasBritainVisa) { // Проверить в одном условии наличие дешёвых билетов и визы
-            System.out.println("Летим через Лондон!");
+        System.out.println("Какой год вы хотите проверить?");
+        int year = scanner.nextInt();
+        // Составьте логическое выражение
+        // Напишите ветвление, где isLeapYear станет условием
+        // Используйте фразы для печати:
+        boolean isLeapYear = (year % 400 == 0 || year % 4 == 0 && year % 100 != 0);
+        if(!isLeapYear) {
+            System.out.println("Согласно григорианскому календарю, этот год - не високосный!");
         } else {
-            System.out.println("Летим через Париж!");
-        }
-    }
-
-    private static String getSeasonByNumber(int monthNumber) {
-        if (monthNumber < 3) {
-            return "Зима";
-        } else if (monthNumber < 6) {
-            return "Весна";
-        } else if (monthNumber < 9) {
-            return "Лето";
-        } else if (monthNumber < 12) {
-            return "Осень";
-        } else {
-            return "Зима";
+            System.out.println("Согласно григорианскому календарю, этот год - високосный!");
         }
     }
 }
