b375f6fc16da38c45d0a295edc301be346a44a58
phzh <p.zhavoronkov@gmail.com>
2025-08-06 17:31:16 +0200

    arrayList. findMax temperature task

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
 /*
-Допишите код: импортируйте и объявите список, а затем заполните его элементами. Добавьте имя «Наталья» после имени «Екатерина» (не меняя строки кода местами). Результат напечатайте.
+Отредактируйте код, который ищет максимальное значение в массиве. Измените массив на список, заполните его элементами и получите с помощью цикла for самое большое значение.
 */
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> femaleNames = new ArrayList<>(); // Создайте список
-        femaleNames.add("Екатерина");
-        femaleNames.add("Мария");
-        femaleNames.add("Анна");
-        femaleNames.add(1, "Наталья"); // Добавьте имя "Наталья"
-        // Распечайте список
-        System.out.println(femaleNames);
+        ArrayList<Double> temperatures = new ArrayList<>();
+        temperatures.add(92.3);
+        temperatures.add(12.4);
+        temperatures.add(74.1);
+        temperatures.add(45.0);
+
+        double max = 0;
+        for (int i = 0; i < temperatures.size(); i++) {
+            if (temperatures.get(i) > max) {
+                max = temperatures.get(i);
+            }
+        }
+        System.out.println("Самая высокая температура: " + max + " °С.");
     }
 }
