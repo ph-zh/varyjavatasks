318685f328ed2f894b08a44e8db4475d0cad162d
phzh <p.zhavoronkov@gmail.com>
2025-08-06 19:11:28 +0200

    arrayList. task with zoo animals prioritization through methods add,size,get

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,33 @@
 /*
-В списке speeds хранится информация о скоростях автомобиля на разных участках дороги. С помощью короткой формы цикла for и метода size() посчитайте среднюю скорость.
+Вы уже немало знаете о Java и совсем скоро начнёте работать самостоятельно. Потренируйтесь это делать.
+Напишите от начала и до конца код небольшого приложения для маркетингового отдела зоопарка.
+Оно должно печатать список и количество животных, кормления которых смогут увидеть посетители, а также расписание сеансов.
+Добавьте в список шиншиллу, крокодила, льва, медведя и слона — строго в таком порядке.
+При составлении расписания учитывайте, что первым кормят крокодила, затем слона, ещё через час — шиншиллу, в полдень — льва, а последним — медведя.
+Для печати животных используйте сокращённый цикл for. Весь необходимый текст вы найдёте в комментариях — от вас нужен только код.
 */
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> speeds = new ArrayList<>(); // Объявите список
-        speeds.add(120);
-        speeds.add(75);
-        speeds.add(42);
-        speeds.add(60);
-        speeds.add(110);
-        speeds.add(20);
+        ArrayList<String> animals = new ArrayList<>();
+        animals.add("Шиншилла");
+        animals.add("Крокодил");
+        animals.add("Лев");
+        animals.add("Медведь");
+        animals.add("Слон");
 
-        int sum = 0;
-        for (Integer speed : speeds) {
-            sum += speed;  // Сложите все значения в списке
+        System.out.println("Сегодня в зоопарке можно увидеть кормления " + animals.size() + " животных:");
+        System.out.println("Это будут:");
+        for(String animal: animals) {
+            System.out.println(animal);
         }
 
-        int averageSpeed = sum / speeds.size(); // Вычислите среднюю скорость
-        System.out.println("Средняя скорость равна " + averageSpeed + " км/ч");
+        System.out.println("Расписание кормлений:");
+        System.out.println("В 9:00 - " + animals.get(1));
+        System.out.println("В 10:00 - " + animals.get(4));
+        System.out.println("В 11:00 - " + animals.get(0));
+        System.out.println("В 12:00 - " + animals.get(2));
+        System.out.println("В 13:00 - " + animals.get(3));
     }
 }
