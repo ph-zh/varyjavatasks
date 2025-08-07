9b0181cd90af43c944dd3d3d4adf02d7f4adfabf
phzh <p.zhavoronkov@gmail.com>
2025-08-07 08:53:31 +0200

    arrayList. method contain example

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,20 @@
 /*
-Вы уже немало знаете о Java и совсем скоро начнёте работать самостоятельно. Потренируйтесь это делать.
-Напишите от начала и до конца код небольшого приложения для маркетингового отдела зоопарка.
-Оно должно печатать список и количество животных, кормления которых смогут увидеть посетители, а также расписание сеансов.
-Добавьте в список шиншиллу, крокодила, льва, медведя и слона — строго в таком порядке.
-При составлении расписания учитывайте, что первым кормят крокодила, затем слона, ещё через час — шиншиллу, в полдень — льва, а последним — медведя.
-Для печати животных используйте сокращённый цикл for. Весь необходимый текст вы найдёте в комментариях — от вас нужен только код.
+
 */
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> animals = new ArrayList<>();
-        animals.add("Шиншилла");
-        animals.add("Крокодил");
-        animals.add("Лев");
-        animals.add("Медведь");
-        animals.add("Слон");
-
-        System.out.println("Сегодня в зоопарке можно увидеть кормления " + animals.size() + " животных:");
-        System.out.println("Это будут:");
-        for(String animal: animals) {
-            System.out.println(animal);
+        ArrayList<Double> expenses = new ArrayList<>();
+        expenses.add(120.23);
+        expenses.add(290.32);
+        expenses.add(420.03);
+        boolean isExp = expenses.contains(290.32); // Проверяем наличие элемента
+        if (isExp == false) {
+            expenses.add(1,290.32); // Если элемента нет - его нужно добавить
+            System.out.println("Трата " + expenses.get(1) + " рублей добавлена!");
+        } else {
+            System.out.println("Все расходы учтены!");
         }
-
-        System.out.println("Расписание кормлений:");
-        System.out.println("В 9:00 - " + animals.get(1));
-        System.out.println("В 10:00 - " + animals.get(4));
-        System.out.println("В 11:00 - " + animals.get(0));
-        System.out.println("В 12:00 - " + animals.get(2));
-        System.out.println("В 13:00 - " + animals.get(3));
     }
 }
