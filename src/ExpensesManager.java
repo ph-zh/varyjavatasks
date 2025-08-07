6871adbec9d75f483f994e1633d83ff778839cf4
phzh <p.zhavoronkov@gmail.com>
2025-08-07 17:50:53 +0200

    finance advisor. Refactoring to ArrayList. Class expensesManager method saveExpense updated

--- src/ExpensesManager.java
+++ src/ExpensesManager.java
@@ -1,16 +1,18 @@
 // Импортируйте ArrayList
+import java.util.ArrayList;
 
 public class ExpensesManager {
-    double[] expenses; // Замените массив списком
+    // Замените массив списком
+    ArrayList<Double> expenses;
 
     ExpensesManager() {
-        expenses = new double[7]; // Создайте список в конструкторе
+        expenses = new ArrayList<>(); // Создайте список в конструкторе
     }
 
     // Номер дня больше не нужен
-    double saveExpense(double moneyBeforeSalary, double expense, int day) {
+    double saveExpense(double moneyBeforeSalary, double expense) {
         moneyBeforeSalary = moneyBeforeSalary - expense;
-        expenses[day - 1] = expenses[day - 1] + expense; // Эту строку нужно убрать
+        expenses.add(expense);
         System.out.println("Значение сохранено! Ваш текущий баланс в рублях: " + moneyBeforeSalary);
         if (moneyBeforeSalary < 1000) {
             System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
@@ -19,16 +21,16 @@ public class ExpensesManager {
     }
 
     void printAllExpenses() {
-        for (int i = 0; i < expenses.length; i++) {
-            System.out.println("Трата № " + (i + 1) + ". Потрачено " + expenses[i] + " рублей");
+        for (int i = 0; i < expenses.size(); i++) {
+            System.out.println("Трата № " + (i + 1) + ". Потрачено " + expenses.get(i) + " рублей");
         }
     }
 
     double findMaxExpense() {
         double maxExpense = 0;
-        for (int i = 0; i < expenses.length; i++) { // Используйте сокращённую форму цикла
-            if (expenses[i] > maxExpense) {
-                maxExpense = expenses[i];
+        for (Double expense: expenses) {
+            if(maxExpense < expense) {
+                maxExpense = expense;
             }
         }
         return maxExpense;
