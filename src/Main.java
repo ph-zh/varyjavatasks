1e6658221818fafcaf7a5ba3b79241db920f9bfa
phzh <p.zhavoronkov@gmail.com>
2025-08-11 01:39:06 +0200

    HashMap with Arraylist example

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,28 @@
 
 */
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, String> officeTool = new HashMap<>();
-        officeTool.put("S234", "Большой степлер");
-        officeTool.put("P342", "Чёрно-белый принтер из коридора");
-        officeTool.put("N845", "Острые ножницы");
-        System.out.println(officeTool);
+        HashMap<String, ArrayList<Double>> menu = new HashMap<>(); //создаём хеш-таблицу
+        ArrayList<Double> mohitoPrice = new ArrayList<>(); //создаём список с ценами для коктейля
+        mohitoPrice.add(350.0);  //добавляем в список цену в рублях
+        mohitoPrice.add(15.50);  //добавляем в список цену в долларах
+        mohitoPrice.add(13.20);  //добавляем в список цену в евро
+        menu.put("Коктейль Mojito", mohitoPrice); //добавляем коктейль и список ценами в хеш-таблицу
+
+        ArrayList<Double> tiramisuPrice = new ArrayList<>();  //создаём список с ценами для тирамису
+        tiramisuPrice.add(120.0);
+        tiramisuPrice.add(4.00);
+        tiramisuPrice.add(3.20);
+        menu.put("Тирамису", tiramisuPrice); //добавляем тирамису и список с ценами в хеш-таблицу
+
+        ArrayList<Double> ramenPrice = new ArrayList<>(); //создаём список с ценами для рамена
+        ramenPrice.add(230.0);
+        ramenPrice.add(8.50);
+        ramenPrice.add(7.00);
+        menu.put("Рамен", ramenPrice); //добавляем рамен и список с ценами в хеш-таблицу
+        System.out.println(menu);
     }
 }
