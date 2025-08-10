fcc3b6e0bfe7ac4afb42fca54d5f806cca8f083f
phzh <p.zhavoronkov@gmail.com>
2025-08-11 01:51:12 +0200

    HashMap with Arraylist. Public holidays task

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,44 @@
 /*
-
+Напишите код хеш-таблицы государственных праздников stateHolidays, которая будет содержать месяцы и праздничные даты:
+Январь — 1, 7;
+Февраль — 23;
+Март — 8;
+Май — 1, 9;
+Июнь — 12;
+Ноябрь — 4.
 */
 import java.util.HashMap;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, ArrayList<Double>> menu = new HashMap<>(); //создаём хеш-таблицу
-        ArrayList<Double> mohitoPrice = new ArrayList<>(); //создаём список с ценами для коктейля
-        mohitoPrice.add(350.0);  //добавляем в список цену в рублях
-        mohitoPrice.add(15.50);  //добавляем в список цену в долларах
-        mohitoPrice.add(13.20);  //добавляем в список цену в евро
-        menu.put("Коктейль Mojito", mohitoPrice); //добавляем коктейль и список ценами в хеш-таблицу
+        HashMap<String, ArrayList<Integer>> stateHolidays = new HashMap<>();
+        ArrayList<Integer> janDays = new ArrayList<>();
+        janDays.add(1);
+        janDays.add(7);
+        stateHolidays.put("Январь", janDays);
 
-        ArrayList<Double> tiramisuPrice = new ArrayList<>();  //создаём список с ценами для тирамису
-        tiramisuPrice.add(120.0);
-        tiramisuPrice.add(4.00);
-        tiramisuPrice.add(3.20);
-        menu.put("Тирамису", tiramisuPrice); //добавляем тирамису и список с ценами в хеш-таблицу
+        ArrayList<Integer> febDays = new ArrayList<>();
+        febDays.add(23);
+        stateHolidays.put("Февраль", febDays);
 
-        ArrayList<Double> ramenPrice = new ArrayList<>(); //создаём список с ценами для рамена
-        ramenPrice.add(230.0);
-        ramenPrice.add(8.50);
-        ramenPrice.add(7.00);
-        menu.put("Рамен", ramenPrice); //добавляем рамен и список с ценами в хеш-таблицу
-        System.out.println(menu);
+        ArrayList<Integer> marchDays = new ArrayList<>();
+        marchDays.add(8);
+        stateHolidays.put("Март", marchDays);
+
+        ArrayList<Integer> mayDays = new ArrayList<>();
+        mayDays.add(1);
+        mayDays.add(9);
+        stateHolidays.put("Май", mayDays);
+
+        ArrayList<Integer> juneDays = new ArrayList<>();
+        juneDays.add(12);
+        stateHolidays.put("Июнь", juneDays);
+
+        ArrayList<Integer> novDays = new ArrayList<>();
+        novDays.add(4);
+        stateHolidays.put("Ноябрь", novDays);
+
+        System.out.println(stateHolidays);
     }
 }
