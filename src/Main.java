1de63231bf8bbf4fab35591eacabb9bce89c2e4a
phzh <p.zhavoronkov@gmail.com>
2025-08-11 13:29:52 +0200

    HashMap. Method get Object value example

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,20 @@
 /*
-Напишите код хеш-таблицы государственных праздников stateHolidays, которая будет содержать месяцы и праздничные даты:
-Январь — 1, 7;
-Февраль — 23;
-Март — 8;
-Май — 1, 9;
-Июнь — 12;
-Ноябрь — 4.
+В таблице хранятся названия стран и их столицы. Напечатайте названия столиц Аргентины и Норвегии.
 */
 import java.util.HashMap;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, ArrayList<Integer>> stateHolidays = new HashMap<>();
-        ArrayList<Integer> janDays = new ArrayList<>();
-        janDays.add(1);
-        janDays.add(7);
-        stateHolidays.put("Январь", janDays);
+        HashMap<String, String> countriesCapitals = new HashMap<>();
+        countriesCapitals.put("Франция", "Париж");
+        countriesCapitals.put("Аргентина", "Буэнос-Айрес");
+        countriesCapitals.put("Россия", "Москва");
+        countriesCapitals.put("Америка", "Вашингтон");
+        countriesCapitals.put("Япония", "Токио");
+        countriesCapitals.put("Норвегия", "Осло");
 
-        ArrayList<Integer> febDays = new ArrayList<>();
-        febDays.add(23);
-        stateHolidays.put("Февраль", febDays);
+        System.out.println("Столица Аргентины: " + countriesCapitals.get("Аргентина"));
+        System.out.println("Столица Норвегии: " + countriesCapitals.get("Норвегия"));
 
-        ArrayList<Integer> marchDays = new ArrayList<>();
-        marchDays.add(8);
-        stateHolidays.put("Март", marchDays);
-
-        ArrayList<Integer> mayDays = new ArrayList<>();
-        mayDays.add(1);
-        mayDays.add(9);
-        stateHolidays.put("Май", mayDays);
-
-        ArrayList<Integer> juneDays = new ArrayList<>();
-        juneDays.add(12);
-        stateHolidays.put("Июнь", juneDays);
-
-        ArrayList<Integer> novDays = new ArrayList<>();
-        novDays.add(4);
-        stateHolidays.put("Ноябрь", novDays);
-
-        System.out.println(stateHolidays);
     }
 }
