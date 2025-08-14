f611acfbb062ffedfdb25a465b2670c156244128
phzh <p.zhavoronkov@gmail.com>
2025-08-14 18:58:40 +0200

    HashMap. Method contains task

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,19 @@
 /*
-В таблице собраны имена клиентов зоомагазина и сумма, на которую они делали заказы. Посчитайте, сколько всего денег было потрачено на питомцев.
+
 */
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, Double> orders = new HashMap<>();
-        orders.put("Иван И.", 4345.5);
-        orders.put("Ольга С.", 76564.43);
-        orders.put("Александр Т.", 1234.86);
-        orders.put("Александр Р.", 23432.87);
-        orders.put("Екатерина О.", 1034753.6);
-        orders.put("Ярослав В.", 450.0);
+        HashMap<String, String> statescapitals = new HashMap<>();
+        statescapitals.put("Россия", "Москва");
+        statescapitals.put("Франция", "Париж");
+        statescapitals.put("Италия", "Рим");
 
-        double sum = 0; // Объявите переменную, где будет сохранена общая сумма
-        for (Double order : orders.values()) { // Пройдитесь в цикле по значениям
-            sum = sum + order;
-        }
+        System.out.println(statescapitals.containsKey("Италия")); // отобразит  true
+        System.out.println(statescapitals.containsKey("Германия"));  // отобразит  false
 
-        System.out.println("Всего было совершено заказов на сумму: " + sum);
+        System.out.println(statescapitals.containsValue("Париж")); // отобразит  true
+        System.out.println(statescapitals.containsValue("Пекин")); // отобразит  false
     }
 }
