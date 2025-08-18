511b4c1c4bf986bc751fd09b0b8f474ff3d69bd9
phzh <p.zhavoronkov@gmail.com>
2025-08-18 21:37:02 +0200

    Finance advisor refactoring with HashMap for class ExpensesManager

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,87 @@
 /*
-В хеш-таблице customersOrders класса OrdersManager собрана информация о клиентах
-зоомагазина (это ключи) и их заказах. Дополните код методов:
- 1/ В printAllOrders() и getOrdersSum(), которые печатают список всех заказов и
- возвращают их общую сумму, вызовите нужные методы HashMap.
- 2/  В методе printCustomerOrders(String customerName) перед печатью заказов
- конкретного клиента добавьте проверку наличия его имени в таблице.
- Это нужно, чтобы избежать ошибки ссылки на null.
- 3/ Метод getMaxOrderCustomerName() должен возвращать имя клиента, который потратил в зоомагазине больше всего.
- Переменные для хранения имени клиента и максимальной суммы заказов мы объявили и инициализировали за вас.
- Числовое переменной присвоили 0, а строковой — пустую строку "". Вам нужно дописать логику работы метода.
- Вы уже не раз искали максимум в массивах или списках. Здесь получится очень похоже!
- 4/ Метод removeUnprofitableOrders должен удалять из таблицы клиентов, сумма заказов которых строго меньше 5000.
+Замените список expenses в классе ExpensesManager на хеш-таблицу expensesByCategories.
+Ключами в ней станут названия категорий, а значениями — списки трат. После этого нужно поменять логику работы методов:
+ 1/ Сделайте так, чтобы при добавлении траты теперь учитывалась её категория. В метод saveExpense добавьте параметр category.
+Наличие категории нужно проверить. Если она есть, в её список должна добавляться новая трата.
+В обратном случае нужно создать новый список трат и добавить его вместе с названием категории в таблицу.
+
+ 2/ Замените имя метода printAllExpenses() на printAllExpensesByCategories().
+И исправьте его функционал. Он должен печатать категории и расходы в них. Пример того, как это может выглядеть:
+Дом
+200.30
+122.65
+302.87
+33.97
+Здоровье
+700.40
+Еда
+1300.56
+800.00
+
+ 3/ Имя метода findMaxExpense() замените на findMaxExpenseInCategory(). Этот метод должен принимать в качестве аргумента название категории. Если категория есть в таблице, то метод должен найти в её списке максимальную трату. Если нет — то сообщить об этом пользователю.
+
+ 4/ Исправьте код в методе removeAllExpenses().
+
+После рефакторинга ExpensesManager поменяйте вызов методов в классе Praktikum. Мы уже внесли там нужные изменения в меню и добавили печать вопросов. 
  */
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        OrdersManager ordersManager = new OrdersManager();
 
-        ordersManager.printAllOrders();
-        System.out.println("Всего заказов на сумму: " + ordersManager.getOrdersSum());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Сколько денег у вас осталось до зарплаты?");
+        double moneyBeforeSalary = scanner.nextDouble();
+        System.out.println("Сколько дней до зарплаты?");
+        int daysBeforeSalary = scanner.nextInt();
 
-        String maxOrderCustomerName = ordersManager.getMaxOrderCustomerName();
-        System.out.println("Самая большая сумма заказов у " + maxOrderCustomerName);
-        ordersManager.printCustomerOrders(maxOrderCustomerName);
+        Converter converter = new Converter(78.5, 88.7, 0.75);
+        DinnerAdvisor dinnerAdvisor = new DinnerAdvisor();
+        ExpensesManager expensesManager = new ExpensesManager();
 
-        // ordersManager.removeUnprofitableOrders();
+        while (true) {
+            printMenu();
+            int command = scanner.nextInt();
+
+            if (command == 1) {
+                System.out.println("Ваши сбережения: " + moneyBeforeSalary + " RUB");
+                System.out.println("В какую валюту хотите конвертировать? Доступные варианты: 1 - USD, 2 - EUR, 3 - JPY.");
+                int currency = scanner.nextInt();
+                converter.convert(moneyBeforeSalary, currency);
+            } else if (command == 2) {
+                dinnerAdvisor.getAdvice(moneyBeforeSalary, daysBeforeSalary);
+            } else if (command == 3) {
+                System.out.println("Введите размер траты:");
+                double expense = scanner.nextDouble();
+                System.out.println("К какой категории относится трата?");
+                String category = scanner.next();
+                moneyBeforeSalary = ...
+            } else if (command == 4) {
+                expensesManager.printAllExpensesByCategories();
+            } else if (command == 5) {
+                System.out.println("В какой категории искать?");
+                String category = scanner.next();
+                System.out.println("Самая большая трата в категории " + category + " составила "
+                        + ... + " руб.");
+            } else if (command == 6) {
+                expensesManager.removeAllExpenses();
+            } else if (command == 0) {
+                System.out.println("Выход");
+                break;
+            } else {
+                System.out.println("Извините, такой команды пока нет.");
+            }
+        }
+    }
+
+    public static void printMenu() {
+        System.out.println("Что вы хотите сделать? ");
+        System.out.println("1 - Конвертировать валюту");
+        System.out.println("2 - Получить совет");
+        System.out.println("3 - Ввести трату");
+        System.out.println("4 - Показать траты по категориям");
+        System.out.println("5 - Показать самую большую трату в выбранной категории");
+        System.out.println("6 - Очистить таблицу трат");
+        System.out.println("0 - Выход");
     }
 }
