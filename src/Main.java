cda7821b2e24d46fc7d20778ca74892b29720d7d
phzh <p.zhavoronkov@gmail.com>
2025-08-25 20:53:46 +0200

    Bobo pizza task. To check scope visibility

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,50 @@
 /*
-
+Перед вами программа-диспетчер для организации работы курьеров «БобоПицца». Она считает время на доставку и предлагает заказ свободному курьеру.
+На дистанции более 10 км отправляются курьеры на машинах, на дистанции менее 2 км курьеры-пешеходы, остальные заказы выполняют курьеры-велосипедисты.
+Автор программы что-то напутал с областями видимости переменных. Исправьте код программы, чтобы она работала корректно.
  */
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите количество действий => ");
-        int iterationsCount = scanner.nextInt();
-// ----------- начало видимого кода сниппета
-        for (int counter = 0; counter < iterationsCount; counter++) {
-            System.out.println("1. Фундук");
-            System.out.println("2. Арахис");
-            System.out.println("3. Пекан");
-            System.out.print("Введите тип орехов => ");
-            int nutType = scanner.nextInt();
-            System.out.print("Введите количество грамм => ");
-            if (nutType == 1) {
-                int weight = scanner.nextInt();
-                calculateAndShowNutPrice("фундука", weight, 1680);
-            } else if (nutType == 2) {
-                int weight = scanner.nextInt();
-                calculateAndShowNutPrice("арахиса", weight, 360);
-            } else if (nutType == 3) {
-                int weight = scanner.nextInt();
-                calculateAndShowNutPrice("пекана", weight, 1299);
-            }
-            System.out.println(nutType);
-        }
-// ----------- конец видимого кода сниппета
+    private Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        List<BikeTimeCalculator> bikes = new ArrayList<BikeTimeCalculator>() {{
+            add(new BikeTimeCalculator("Иван"));
+            add(new BikeTimeCalculator("Василий"));
+            add(new BikeTimeCalculator("Инга"));
+        }};
+        List<CarTimeCalculator> cars = new ArrayList<CarTimeCalculator>() {{
+            add(new CarTimeCalculator("Ольга"));
+            add(new CarTimeCalculator("Пётр"));
+        }};
+        List<HumanTimeCalculator> humans = new ArrayList<HumanTimeCalculator>() {{
+            add(new HumanTimeCalculator("Анна"));
+            add(new HumanTimeCalculator("Дмитрий"));
+            add(new HumanTimeCalculator("Алексей"));
+        }};
+        new BoboPizza().loop();
     }
 
-    public static void calculateAndShowNutPrice(String nutType, int weight, double price) {
-        System.out.println("Вычисляем стоимость " + weight + " грамма(ов) " + nutType);
-        double total = (weight * price) / 1000;
-        System.out.println("Стоимость " + weight + " грамма(ов) " + nutType + " = " + total + "₽");
+    private void loop() {
+        CourierSelector selector = new CourierSelector(bikes, cars, humans);
+
+        DeliveryInfo delivery = null;
+
+        do {
+            System.out.println("Введите расстояние в метрах => ");
+            int distance = scanner.nextInt();
+            delivery = selector.selectDelivery(distance);
+            if (delivery != null) {
+                System.out.println(
+                        "Пицца будет доставлена через " + delivery.getTime() + " минут(у,ы)."
+                );
+                System.out.println("Ваш курьер - " + delivery.getName() + ".");
+            } else {
+                System.out.println("Доступных курьеров не найдено");
+            }
+        } while (delivery != null);
     }
 }
