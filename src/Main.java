e91da07555f71ecda2c84df194900f194bfb1e85
ph.zh <p.zhavoronkov@gmail.com>
2025-09-05 12:39:39 +0200

    Override task

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,25 @@
 /*
-Вам нужно снять наличные в банкомате, но он сломался и выводит только консоль с недописанным кодом.
-По счастливой случайности — на Java. Допишите код — реализуйте методы в классе BankAccount.
-Чтобы установить и считать значение суммы денег на счёте moneyAmount, вам понадобятся get- и set-методы.
-Чтобы снять деньги со счёта и обнулить его — метод withdrawAll(), который должен обнулять счёт и печатать
-количество выданных денег в формате: Со счёта снято 1000 р. Все методы должны иметь самый широкий уровень доступа.
-В результате запуска программы в консоли должно появиться:
-
-- Количество денег на счету - 1000 р. (сумма может быть любой)
-- Со счёта снято 1000 р.
-- Количество денег на счету - 0 р.
+Исправьте метод playHamlet() так, чтобы исполнитель performer прочитал строчку из
+монолога Гамлета — «Быть иль не быть? Вот в чём вопрос?».
  */
 
-public class Main {
-    public static void main(String[] args) {
-        BankAccount bankAccount = new BankAccount();
-        long moneyAmount = 1000; // передайте в банкомат сумму на счету
-        bankAccount.setMoneyAmount(moneyAmount);
-        System.out.println("Количество денег на счету - " + bankAccount.getMoneyAmount() + " р.");
-        bankAccount.withdrawAll(); // вызовите метод вывода средств
-        System.out.println("Количество денег на счету - " + bankAccount.getMoneyAmount() + " р.");
+class Actor {
+
+    void play() {
+        System.out.println("Гул затих. Я вышел на подмостки.");
     }
 }
 
-class BankAccount {
-    private long moneyAmount;
-
-    // допишите код методов
-    // используйте параметр newMoneyAmount для установки нового значения
-    public long getMoneyAmount() {
-        return moneyAmount;
+class Hamlet extends Actor {
+    @Override
+    void play() {
+        System.out.println("Быть или не быть? Вот в чём вопрос?");
     }
+}
 
-    public void setMoneyAmount(long newMoneyAmount) {
-        moneyAmount = newMoneyAmount;
-    }
-
-    public void withdrawAll() {
-        System.out.println("Со счёта снято " + moneyAmount + " р.");
-        moneyAmount -= moneyAmount;
+public class Main {
+    public static void main(String[] args) {
+        Hamlet performer = new Hamlet();
+        performer.play();
     }
 }
