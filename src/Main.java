750919ed9a9200fa77958975a6f1f0ab7d5dee58
ph.zh <p.zhavoronkov@gmail.com>
2025-09-05 13:22:51 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,9 @@
 /*
-Исправьте метод playHamlet() так, чтобы исполнитель performer прочитал строчку из
-монолога Гамлета — «Быть иль не быть? Вот в чём вопрос?».
+
  */
-
-class Actor {
-
-    void play() {
-        System.out.println("Гул затих. Я вышел на подмостки.");
-    }
-}
-
-class Hamlet extends Actor {
-    @Override
-    void play() {
-        System.out.println("Быть или не быть? Вот в чём вопрос?");
-    }
-}
 
 public class Main {
     public static void main(String[] args) {
-        Hamlet performer = new Hamlet();
-        performer.play();
+
     }
 }
