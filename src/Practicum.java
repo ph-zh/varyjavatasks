c50ea9bf2c1b35e38fb3925e0a26ec6d14dcfb02
phzh <p.zhavoronkov@gmail.com>
2025-09-07 22:11:55 +0200

    fix. Constructor LiteratureTeacher updated with key word super

--- src/Practicum.java
+++ src/Practicum.java
@@ -1,51 +1,27 @@
 class Teacher {
-    int numberOfLessons; // число уроков
-    int numberOfPupils; // число учеников
-    String name; // имя учителя
+    String name;
 
-    // конструктор класса-родителя
-    public Teacher(int newNumberOfLessons,
-                   int newNumberOfPupils,
-                   String newName) {
-        numberOfLessons = newNumberOfLessons;
-        numberOfPupils = newNumberOfPupils;
+    public Teacher(String newName) {
         name = newName;
     }
 }
 
-class GymTeacher extends Teacher {
-    int basketballLessons; // уроки по баскетболу
-    int swimmingLessons; // уроки по плаванию
-    int freeLessons; // уроки без строгой программы
+class LiteratureTeacher extends Teacher {
+    int pushkinLessons;
+    int tolstojLessons;
 
-    // конструктор в подклассе принимает и свои параметры, и параметры суперкласса
-    public GymTeacher(int newNumberOfLessons,
-                      int newNumberOfPupils,
-                      String newName,
-                      int numberBasketballLessons,
-                      int numberSwimmingLessons) {
-        // сначала вызываем конструктор класса-родителя
-        super(newNumberOfLessons, newNumberOfPupils, newName);
-        // инициализируем новые поля
-        basketballLessons = numberBasketballLessons;
-        swimmingLessons = numberSwimmingLessons;
-        // поле freeLessons вычисляется на основе заданных параметров
-        freeLessons = newNumberOfLessons - numberBasketballLessons - numberSwimmingLessons;
-    }
-
-    public void printInfo() {
-        System.out.println("ФИО учителя - " + name);
-        System.out.println("Число учеников - " + numberOfPupils);
-        System.out.println("Число уроков - " + numberOfLessons);
-        System.out.println("Уроков по плаванию - " + swimmingLessons);
-        System.out.println("Уроков по баскетболу - " + basketballLessons);
-        System.out.println("Свободных уроков - " + freeLessons);
+    public LiteratureTeacher(int newPushkinLessons,int newTolstojLessons,String name) {
+        super(name);
+        pushkinLessons = newPushkinLessons;
+        tolstojLessons = newTolstojLessons;
     }
 }
 
 public class Practicum {
     public static void main(String[] args) {
-        GymTeacher teacher = new GymTeacher(12, 30, "Олег Евгеньевич Фомин", 5, 4);
-        teacher.printInfo();
+        LiteratureTeacher teacher = new LiteratureTeacher(12, 43, "Мария Ивановна Петрова");
+        System.out.println("Уроков по Пушкину - " + teacher.pushkinLessons);
+        System.out.println("Уроков по Толстому - " + teacher.tolstojLessons);
+        System.out.println("Учитель - " + teacher.name);
     }
 }
