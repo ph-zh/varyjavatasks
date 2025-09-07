48a0f943d031abaaa757f432258eb6e1a4890e2c
phzh <p.zhavoronkov@gmail.com>
2025-09-07 22:15:16 +0200

    Spacecraft task example

---
+++ src/yandex/practicum/space/station/Zvezda.java
@@ -0,0 +1,13 @@
+package yandex.practicum.space.station;
+
+class Zvezda extends Zarya {
+
+    public Zvezda() { // доступ к изменению конструктора запрещён
+        super();
+        energyConsumption = 44;
+        activateModule();
+    }
+    // доступ к созданию новых конструкторов запрещён
+
+    // разрешено переопределение методов
+}
