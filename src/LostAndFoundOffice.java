3f91db7b1050e0feefba2d3feb586e1dc385e778
phzh <p.zhavoronkov@gmail.com>
2025-09-12 19:43:16 +0200

    fix method check

--- src/LostAndFoundOffice.java
+++ src/LostAndFoundOffice.java
@@ -8,18 +8,21 @@ import java.util.ArrayList;
 
 public class LostAndFoundOffice {
     // создайте список things
-    static ArrayList<Object> things = new ArrayList<>();
+    ArrayList<Object> things = new ArrayList<>();
 
     // реализуйте метод put()
-    public static void put(Object object) {
+    void put(Object object) {
         things.add(object);
     }
 
     // реализуйте метод check()
-    public static boolean check(Object target) {
+
+    boolean check(Object target) {
         for (Object object : things) { // логика проверки вещи в списке
             if (object.equals(target)) {
                 return true;
+            } else if(object == null) {
+                return false;
             }
         }
         return false;
