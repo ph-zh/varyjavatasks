ccd0e9545d5dd732e16e85f65c3e0a07d907a324
phzh <p.zhavoronkov@gmail.com>
2025-09-12 16:48:43 +0200

    sizeof Object task example

--- src/Practicum.java
+++ src/Practicum.java
@@ -1,66 +1,35 @@
 /*
-Чтобы выполнить этот квиз, скопируйте код ниже в среду разработки IDEA.
-Добавьте в него всё что нужно, чтобы программа заработала. Используйте комбинацию alt+Insert или команду Generate....
-У вас также есть уникальная возможность научить лису говорить так, как считаете нужным.
+
  */
-
-class Animal {
-    private double weight;
-    private int age;
-
-    public Animal() {
-        this.weight = 0.0d;
-        this.age = 0;
-    }
-
-    public String say() {
-        return "Мяу, гав, хрю, кря!";
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-}
-
-class Fox extends Animal {
-    private String color;
-
-    public Fox(String color) {
-        this.color = color;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    // научите лису говорить
-    @Override
-    public String say() {
-        return super.say();
-    }
-
-}
 
 public class Practicum {
     public static void main(String[] args) {
-        Fox fox = new Fox("рыжий");
-        fox.setWeight(5.93d);
-        fox.setAge(5);
-        System.out.println("Вес лисы - " + fox.getWeight());
-        System.out.println("Возраст лисы - " + fox.getAge());
-        System.out.println("Цвет лисы - " + fox.getColor());
-        System.out.println("Лиса говорит - " + fox.say());
+        Person[] people = { new Person("Пелагея"), new Person("Ферапонт") };
+        int peopleCount = sizeOf(people);
+        System.out.println("В массиве people " + peopleCount + " элемента.");
+
+        String[] names = { "Федот", "Глафира", "Аграфена",  "Епифаний"};
+        int namesCount = sizeOf(names);
+        System.out.println("В массиве names " + namesCount + " элемента.");
+
+        Integer[] numbers = { 42, 24, 45, 34, 23, 43, 54, 65, 43 };
+        int numbersCount = sizeOf(numbers);
+        System.out.println("В массиве numbers " + numbersCount + " элемента.");
+    }
+
+    public static int sizeOf(Object[] array) { // параметр - массив элементов типа Object)
+        int count = 0;
+        for (Object o : array) {
+            count++;
+        }
+        return count;
+    }
+}
+
+class Person {
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
     }
 }
