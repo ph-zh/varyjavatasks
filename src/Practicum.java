fdfd70aa996bbb851effa3a6518ebcfc425ad146
phzh <p.zhavoronkov@gmail.com>
2025-09-14 20:24:24 +0200

    classes comparison task

--- src/Practicum.java
+++ src/Practicum.java
@@ -1,40 +1,36 @@
 /*
-Допишите код класса — бюро находок LostAndFoundOffice. В нём должен быть список things для учёта потерянных вещей, метод put() для добавления вещи в список, метод check() — для проверки её наличия.
-В зависимости от результата check() должен возвращать true или false. Методы put() и check() должны быть универсальными — принимать в качестве параметра объекты любых классов.
-Не забудьте предусмотреть ситуацию, если в check() передана пустая ссылка.
- */
 
-class Ball { }      // Класс "мяч"
-class Accordion { } // Класс "аккордеон"
-class Hat { }       // Класс "шляпа"
-class Umbrella { }  // Класс "зонт"
+ */
+class Book {
+    public String title;
+    public String author;
+    public int pagesNumber;
+
+    public Book(String title, String author, int pagesNumber) {
+        this.title = title;
+        this.author = author;
+        this.pagesNumber = pagesNumber;
+    }
+}
 
 public class Practicum {
-    // Бюро находок
-    static LostAndFoundOffice lostAndFound = new LostAndFoundOffice();
-
     public static void main(String[] args) {
-        Ball ball = new Ball();
-        Accordion accordion = new Accordion();
-        Umbrella umbrella = new Umbrella();
-        Hat hat = new Hat();
+        // сохраняем одно и то же число в две переменные
+        int variable1 = 42;
+        int variable2 = 42;
 
-        // Сдаём в бюро находок найденные вещи
-        lostAndFound.put(ball);
-        lostAndFound.put(accordion);
-        lostAndFound.put(umbrella);
+        boolean result = variable1 == variable2; // сравниваем значения переменных
+        System.out.println(result); // значения равны
 
-        checkObject(accordion, "Aккордеон");
-        checkObject(ball, "Мяч");
-        checkObject(umbrella, "Зонт");
-        checkObject(hat, "Шляпа");
-    }
+        String title = "Java практикум для начинающих";
+        String author = "Иван Сеньёров";
+        int pagesNumber = 777;
 
-    private static void checkObject(Object object, String description) {
-        if(lostAndFound.check(object)) {
-            System.out.println('\'' + description + "' нашёлся!");
-        } else {
-            System.out.println('\'' + description + "' в бюро находок никто не приносил :(");
-        }
+        // передаём одни и те же данные двум объектам
+        Book book1 = new Book(title, author, pagesNumber);
+        Book book2 = new Book(title, author, pagesNumber);
+
+        boolean result2 = book1 == book2; // сравниваем значения объектных переменных
+        System.out.println(result2); // получили некорректный результат
     }
 }
