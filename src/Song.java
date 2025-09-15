4e5fc7b780c647f9324f3c3a6aadb5c9eca6070e
ph.zh <p.zhavoronkov@gmail.com>
2025-09-15 09:51:01 +0200

    override equals objects task template

---
+++ src/Song.java
@@ -0,0 +1,17 @@
+import java.util.Objects;
+
+public class Song {
+    public final String title;
+    public final String artist;
+    public final String songwriter;
+
+    public Song(String title, String artist, String songwriter) {
+        this.title = title;
+        this.artist = artist;
+        this.songwriter = songwriter;
+    }
+
+    // переопределите метод equals(Object)
+    ...
+
+}
