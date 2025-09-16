bdf99034dbe3436425303fc069304d9615c981bc
ph.zh <p.zhavoronkov@gmail.com>
2025-09-16 10:52:32 +0200

    classes movie and actor realised with construcotr, euqals, hashCode methods

--- src/Actor.java
+++ src/Actor.java
@@ -1,4 +1,36 @@
+import java.util.Objects;
+
 public class Actor {
     String firstName;
     String lastName;
+
+    public Actor(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true; // проверяем адреса объектов
+        if (obj == null) return false; // проверяем ссылку на null
+        if (this.getClass() != obj.getClass()) return false; // сравниваем классы объектов
+        Actor anotherActor = (Actor) obj; // привели второй объект к классу Movie
+        return Objects.equals(firstName, anotherActor.firstName) && // проверяем поля
+               Objects.equals(lastName, anotherActor.lastName); // примитивы сравниваем через ==
+    }
+
+    @Override
+    public int hashCode() {
+        // вызываем вспомогательный метод и передаём в него нужные поля
+        int hash = 17;
+        if (firstName != null) {
+            hash = hash + firstName.hashCode();
+        }
+        hash = hash * 31;
+
+        if (lastName != null) {
+            hash = hash + lastName.hashCode();
+        }
+        return hash;
+    }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,8 +1,38 @@
+import java.util.Objects; // импортируем класс Objects
+
 public class Movie {
     String title; // название фильма
     int releaseYear; // год выхода на экраны
 
     public String description() { // метод для вывода описания фильма
         return '"' + title + "\" (" + releaseYear + " год)";
+    }
+
+    public Movie(String title, int releaseYear) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true; // проверяем адреса объектов
+        if (obj == null) return false; // проверяем ссылку на null
+        if (this.getClass() != obj.getClass()) return false; // сравниваем классы объектов
+        Movie anotherMovie = (Movie) obj; // привели второй объект к классу Movie
+        return Objects.equals(title, anotherMovie.title) && // проверяем поля
+               (releaseYear == anotherMovie.releaseYear); // примитивы сравниваем через ==
+    }
+
+    @Override
+    public int hashCode() {
+        // вызываем вспомогательный метод и передаём в него нужные поля
+        int hash = 17;
+        if (title != null) {
+            hash = hash + title.hashCode();
+        }
+        hash = hash * 31;
+
+        return hash;
     }
 }
