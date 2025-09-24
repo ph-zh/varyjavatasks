6b7dc8a8c98f5bd1ca41d9957e0f59901783d129
phzh <p.zhavoronkov@gmail.com>
2025-09-24 21:19:44 +0200

    refactor. To replace Array with ArrayList logic in ExpensesManager

--- src/ExpensesManager.java
+++ src/ExpensesManager.java
@@ -1,16 +1,19 @@
-... // Импортируйте ArrayList
+// Импортируйте ArrayList
+import java.util.ArrayList;
 
 public class ExpensesManager {
-    double[] expenses; // Замените массив списком
+    // Замените массив списком
+    ArrayList<Double> expenses;
 
     ExpensesManager() {
-        expenses = new double[7]; // Создайте список в конструкторе
+        expenses = new ArrayList<>(); // Создайте список в конструкторе
     }
 
     // Номер дня больше не нужен
-    double saveExpense(double moneyBeforeSalary, double expense, int day) {
+    double saveExpense(double moneyBeforeSalary, double expense) {
         moneyBeforeSalary = moneyBeforeSalary - expense;
-        expenses[day - 1] = expenses[day - 1] + expense; // Эту строку нужно убрать
+        // Эту строку нужно убрать
+        expenses.add(expense);
         System.out.println("Значение сохранено! Ваш текущий баланс в рублях: " + moneyBeforeSalary);
         if (moneyBeforeSalary < 1000) {
             System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
@@ -19,16 +22,17 @@ public class ExpensesManager {
     }
 
     void printAllExpenses() {
-        for (int i = 0; i < expenses.length; i++) {
-            System.out.println("Трата № " + (i + 1) + ". Потрачено " + expenses[i] + " рублей");
+        for (int i = 0; i < expenses.size(); i++) {
+            System.out.println("Трата № " + (i + 1) + ". Потрачено " + expenses.get(i) + " рублей");
         }
     }
 
     double findMaxExpense() {
         double maxExpense = 0;
-        for (int i = 0; i < expenses.length; i++) { // Используйте сокращённую форму цикла
-            if (expenses[i] > maxExpense) {
-                maxExpense = expenses[i];
+        // Используйте сокращённую форму цикла
+        for (Double expense: expenses) {
+            if (expense > maxExpense) {
+               maxExpense = expense;
             }
         }
         return maxExpense;
