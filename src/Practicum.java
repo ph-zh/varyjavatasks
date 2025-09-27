f479f1cbe1b73bcf5cdb67d39cde6d605caa01c9
phzh <p.zhavoronkov@gmail.com>
2025-09-27 13:12:28 +0200

    Array sort method example

---
+++ src/Practicum.java
@@ -0,0 +1,14 @@
+/*
+
+ */
+import java.util.Arrays;
+
+public class Practicum {
+
+    public static void main(String[] args) {
+        int[] buses = {123, 29, 500, 738, 960};
+        System.out.println(Arrays.toString(buses));
+        Arrays.sort(buses);
+        System.out.println(Arrays.toString(buses));
+    }
+}
