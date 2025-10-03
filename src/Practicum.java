08e2e5606eb6e9eec37742012bb4d0f33234aa0f
phzh <p.zhavoronkov@gmail.com>
2025-10-03 19:40:59 +0200

    eNum with switch operator

--- src/Practicum.java
+++ src/Practicum.java
@@ -1,38 +1,35 @@
 /*
-Перед вами часть программы для хранения списка задач с приоритетом. Приоритет (англ. task priority) может быть:
 
-    высокий (англ. high) — TaskPriority.HIGH,
-    средний (англ. medium) — TaskPriority.MEDIUM,
-    низкий (англ. low) — TaskPriority.LOW.
-
-Вам нужно реализовать поиск задач с наивысшим приоритетом из предложенного списка.
  */
-// импортируйте нужные пакеты
-import java.util.List;
-import java.util.ArrayList;
-
 
 public class Practicum {
 
     public static void main(String[] args) {
-        List<Task> tasks = new ArrayList<>();
-        tasks.add(new Task(TaskPriority.HIGH, "Оплатить интернет."));
-        tasks.add(new Task(TaskPriority.LOW, "Сходить в парикмахерскую."));
-        tasks.add(new Task(TaskPriority.MEDIUM, "Выбрать подарок подруге на ДР."));
-        tasks.add(new Task(TaskPriority.MEDIUM, "Купить билеты в театр."));
-        tasks.add(new Task(TaskPriority.HIGH, "Посетить вебинар по английскому языку."));
-        tasks.add(new Task(TaskPriority.LOW, "Купить пылесос."));
-
-        System.out.println("Задачи с наивысшим приоритетом на сегодня:");
-        filterByPriority(tasks, TaskPriority.HIGH);
-
+        System.out.println(getDownloadLink(DeviceType.RED));
     }
-    // цикл for для поиска задач
-    public static void filterByPriority(List<Task> tasks, TaskPriority taskPriority) {
-        for (Task task : tasks) {
-            if (task.getPriority() == taskPriority) {
-                System.out.println(task.getDescription());
-            }
+
+    public static String getDownloadLink(DeviceType type) {
+        String link = null;
+
+        switch (type) {
+            case ANDROID:
+                link = "https://play.google.com/store/apps/раздельный_сбор_мусора";
+                break;
+            case IOS:
+                link = "https://apps.apple.com/ru/app/раздельный_сбор_мусора";
+                break;
+            case WEB:
+                link = "https://сайт_про_раздельный_сбор_мусора.ru";
+                break;
         }
+
+        return link;
     }
+}
+
+enum DeviceType {
+    ANDROID,
+    IOS,
+    WEB,
+    RED
 }
--- src/TaskPriority.java
+++
@@ -1,5 +0,0 @@
-public enum TaskPriority {
-    HIGH,
-    MEDIUM,
-    LOW
-}
