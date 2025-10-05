44f39d2eb9f08f31dcb9712cfe18faa79cc17670
phzh <p.zhavoronkov@gmail.com>
2025-10-05 18:59:33 +0200

    getPopulationPrecent updated with switch operator

--- src/Practicum.java
+++ src/Practicum.java
@@ -12,24 +12,32 @@ public class Practicum {
     public static String getPopulationPercent(Continent continent) {
         String result;
 
-        if (continent == Continent.ASIA) {
-            result = "59.5%";
-        } else if (continent == Continent.AFRICA) {
-            result = "16.9%";
-        } else if (continent == Continent.NORTH_AMERICA) {
-            result = "7.7%";
-        } else if (continent == Continent.SOUTH_AMERICA) {
-            result = "5.6%";
-        } else if (continent == Continent.ANTARCTICA) {
-            result = "<0.1%";
-        } else if (continent == Continent.EUROPE) {
-            result = "9.7%";
-        } else if (continent == Continent.AUSTRALIA) {
-            result = "0.5%";
-        } else {
-            result = "Такого материка не существует.";
+        switch (continent) {
+            case ASIA:
+                result = "59.5%";
+                break;
+            case AFRICA:
+                result = "16.9%";
+                break;
+            case NORTH_AMERICA:
+                result = "7.7%";
+                break;
+            case SOUTH_AMERICA:
+                result = "5.6%";
+                break;
+            case ANTARCTICA:
+                result = "<0.1%";
+                break;
+            case EUROPE:
+                result = "9.7%";
+                break;
+            case AUSTRALIA:
+                result = "0.5%";
+                break;
+            default:
+                result = "Такого материка не существует.";
+                break;
         }
-
         return result;
     }
 }
