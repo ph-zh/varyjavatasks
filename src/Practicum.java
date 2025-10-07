41822689ad1ccbace0089c0c51aa987d1676dcd7
ph.zh <p.zhavoronkov@gmail.com>
2025-10-07 10:29:28 +0200

    Abstraction methods example

--- src/Practicum.java
+++ src/Practicum.java
@@ -3,10 +3,42 @@
  */
 
 public class Practicum {
+
     public static void main(String[] args) {
-        System.out.println("Теперь класс Figure абстрактный!");
+        Frog frog = new Frog();
+        System.out.println("Привет! Я - зелёная лягушка.");
+        frog.move();
+        frog.eat();
+
+        Toad toad = new Toad();
+        System.out.println("Привет! Я - коричневая жаба.");
+        toad.move();
+        toad.eat();
     }
+
 }
-abstract class Figure {
-    public abstract double getArea();
+abstract class Amphibian {
+
+    public abstract void move();
+
+    public void eat() {
+        System.out.println("Кушаю насекомых");
+    }
+
+}
+class Frog extends Amphibian {
+
+    @Override
+    public void move() {
+        System.out.println("Я передвигаюсь по суше прыжками.");
+    }
+
+}
+class Toad extends Amphibian {
+
+    @Override
+    public void move() {
+        System.out.println("Я важно хожу.");
+    }
+
 }
