263f058a29313627a228c75eb6273f264729cd4b
phzh <p.zhavoronkov@gmail.com>
2025-10-15 14:26:48 +0200

    feat SmartPhone class update with makeCall, sendSMS, sendEmail

--- src/Phone.java
+++ src/Phone.java
@@ -1,11 +1,15 @@
-public ... Phone {
+public abstract class Phone {
     private final String number;
 
     public Phone(String number) {
         this.number = number;
     }
 
-    public final void makeCall(String targetNumber) {
+    public String getNumber() {
+        return number;
+    }
+
+    public void makeCall(String targetNumber) {
         System.out.println("Звоним с номера " + number);
         System.out.println("Набираем номер " + targetNumber + " и звоним по сотовой связи");
         System.out.println("Привет!");
--- src/Smartphone.java
+++ src/Smartphone.java
@@ -1,10 +1,27 @@
 // Допишите реализацию класса Smartphone
-public class Smartphone {
+public class Smartphone extends MobilePhone {
 
-    ...
+    public Smartphone(String number) {
+        super(number);
+    }
 
     public final void sendEmail(String email, String messageText) {
         System.out.println("Напишем другу сообщение " + messageText + " по email " + email);
     }
 
+    public void makeCall(String targetNumber) {
+        System.out.println("Введите email");
+        String email = scanner.nextLine();
+        System.out.println("Введите сообщение");
+        String messageText = scanner.nextLine();
+        sendEmail(email, messageText);
+        System.out.println("Введите имя приложения");
+        String appName = scanner.nextLine();
+        sendEmail(email,messageText);
+        super.sendSms(targetNumber, messageText);
+        System.out.println("Позвоним через приложение " + appName + " по номеру " + targetNumber);
+        System.out.println("Звоним с номера " + getNumber());
+        System.out.println("Набираем номер " + targetNumber + " и звоним по сотовой связи");
+        System.out.println("Привет!");
+    }
 }
