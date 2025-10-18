4c6d0c12dcc0dd8b822af4a29626a3dac15059ff
phzh <p.zhavoronkov@gmail.com>
2025-10-18 13:51:04 +0200

    type casting example

--- src/Practicum.java
+++ src/Practicum.java
@@ -2,23 +2,41 @@
 
  */
 
-import java.util.*;
+interface Document {
+    public String getDocumentNumber();
+}
+
+class RussianPassport implements Document {
+    private final String series;
+    private final String number;
+
+    public RussianPassport(String series, String number) {
+        this.series = series;
+        this.number = number;
+    }
+
+    @Override
+    public String getDocumentNumber() {
+        return series + " " + number;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
+
 
 public class Practicum {
+
     public static void main(String[] args) {
-
-        Queue<String> queue = new PriorityQueue<>(Collections.reverseOrder());
-
-        queue.offer("B");
-        queue.offer("C");
-        queue.offer("A");
-        queue.offer("F");
-        queue.offer("D");
-
-        while(!queue.isEmpty()) {
-            System.out.println(queue.poll());
-        }
-
+        // Java автоматически привела объект класса RussianPassport к типу интерфейса Document
+        Document document = new RussianPassport("1111", "567899");
+        System.out.println(document.getDocumentNumber());
 
     }
+
 }
