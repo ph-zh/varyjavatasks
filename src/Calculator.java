b59ca799a6b416770f22b28c22b6c8812532f7f1
phzh <p.zhavoronkov@gmail.com>
2025-10-18 20:43:47 +0200

    small fix

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,14 +9,14 @@ public class Calculator {
     public static double calculate(List<MediaItem> mediaItems) {
         // Напишите реализацию метода, который будет возвращать общее количество дней,
         // потраченных на просмотр фильмов и сериалов
-        double days = 0;
+        double minutes = 0;
         for (MediaItem mediaItem : mediaItems) {
             if(mediaItem instanceof Movie) {
-                days = days + (double) mediaItem.getRuntime();
+                minutes += mediaItem.getRuntime();
             } else if (mediaItem instanceof Series) {
-                days = days + (double) mediaItem.getRuntime() * mediaItem.getSeriesCount();
+                minutes += mediaItem.getRuntime() * mediaItem.getSeriesCount();
             }
         }
-        return days / 1440;
+        return minutes / 1440;
     }
 }
