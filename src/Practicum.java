2602c25bad7119e009648ad667b5057ec7352325
ph.zh <p.zhavoronkov@gmail.com>
2025-10-20 13:21:20 +0200

    hashmap task example

--- src/Practicum.java
+++ src/Practicum.java
@@ -1,43 +1,21 @@
 /*
 
  */
+import java.util.HashMap;
+import java.util.Map;
+
 public class Practicum {
 
     public static void main(String[] args) {
-        Pair<Integer> pair = new Pair<>(4, 1);
-        System.out.println("Координаты точки с целочисленными координатами:");
-        pair.print();
-        Integer x = pair.getKey();
-        Integer y = pair.getValue();
+        Map<String, Double> catsWithWeight = new HashMap<>();
+        catsWithWeight.put("Пиксель", 4.6);
+        catsWithWeight.put("Космонавт", 5.0);
+        catsWithWeight.put("Потата", 3.2);
+        catsWithWeight.put("Байтик", 2.4);
 
-        System.out.println();
-
-        Pair<String> stringPair = new Pair<>("Content-Type", "application/json");
-        System.out.println("Заголовок HTTP запроса:");
-        stringPair.print();
-        String header = stringPair.getKey();
-        String headerValue = stringPair.getValue();
+        for (String cat : catsWithWeight.keySet()) {
+            System.out.printf("Котик %s весит %f\n",cat, catsWithWeight.get(cat));
+        }
     }
 
-}
-class Pair<T> {
-    public final T key;
-    public final T value;
-
-    public Pair(T key, T value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public void print() {
-        System.out.printf("(%s, %s)", key, value);
-    }
-
-    public T getKey() {
-        return key;
-    }
-
-    public T getValue() {
-        return value;
-    }
 }
