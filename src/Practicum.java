b70db47b8b8e1c38e3d38706903fe0910fe281ef
ph.zh <p.zhavoronkov@gmail.com>
2025-10-23 11:54:03 +0200

    generic upper bound updated with class printer

--- src/Practicum.java
+++ src/Practicum.java
@@ -25,22 +25,22 @@ public class Practicum {
         stringList.add("Hello");
 
         // Этот вариант должен вызывать ошибку компиляции
-        //new Printer<>(stringList).print();
+        // new Printer<>(stringList).print();
     }
 
 }
 
-class Printer... {
-private final List<...> list;
+class Printer<T extends Number> {
+    private final List<T> list;
 
-public Printer(... list) {
-    this.list = list;
-}
-
-public void print() {
-    for (int i = 0; i < list.size(); i++) {
-        double price = list.get(i).doubleValue() / 100;
-        System.out.println("Цена товара: " + price + " руб.");
+    public Printer(List<T> list) {
+        this.list = list;
     }
-}
+
+    public void print() {
+        for (int i = 0; i < list.size(); i++) {
+            double price = list.get(i).doubleValue() / 100;
+            System.out.println("Цена товара: " + price + " руб.");
+        }
+    }
 }
