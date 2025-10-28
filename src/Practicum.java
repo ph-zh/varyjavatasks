98c6aa48df55cadcb16ed4d1deea2c7e1bedf87c
ph.zh <p.zhavoronkov@gmail.com>
2025-10-28 13:18:45 +0100

    algorithm. Array duplicate check

--- src/Practicum.java
+++ src/Practicum.java
@@ -5,14 +5,22 @@
 public class Practicum {
 
     public static void main(String[] args) {
-        int[] array = new int[] {1, 5, 3, 2, 9, 1, 2, 3, 4, 1, 10};
+        int[] array = new int[] {1, 5, 999, 2, 9, 10, 552, 3, 4, 14, 10};
 
-        int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            sum += array[i];
+        boolean duplicateFound = false;
+        for (int i = 0; i < array.length && !duplicateFound; i++) {
+            for (int j = 0; j < array.length; j++) {
+
+                // пропускаем одинаковый индекс, чтобы не сравнить элемент с самим собой
+                if (j == i) continue;
+                if (array[i] == array[j]) {
+                    duplicateFound = true;
+                    break;
+                }
+            }
         }
 
-        System.out.println(sum);
+        System.out.println("Дубликат найден: " + duplicateFound);
     }
 
 }
