f5d2da3ac6b877fc0dd83f566c32e1bc4e7340f1
phzh <p.zhavoronkov@gmail.com>
2025-10-31 19:54:27 +0100

    array list task example

--- src/Practicum.java
+++ src/Practicum.java
@@ -3,31 +3,39 @@
 */
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 
 public class Practicum {
 
     public static void main(String[] args) {
-        final Collection<String> colors = new ArrayList<>();
-        colors.add("Жёлтый");
-        colors.add("Красный");
 
-        final Collection<String> colorsAdditional = new ArrayList<>();
-        colorsAdditional.add("Зелёный");
-        colorsAdditional.add("Жёлтый");
+        List<Long> list = new ArrayList<>(); // создание списка
 
-        colors.addAll(colorsAdditional);
-        System.out.println(colors);
+        // добавление элементов в конец:
+        list.add(0L);
+        list.add(1L);
+        list.add(2L);
+        System.out.println(list);  // список выглядит так - [0, 1, 2]
 
-        final Collection<String> greenAndRed = new ArrayList<>();
-        greenAndRed.add("Зелёный");
-        greenAndRed.add("Красный");
-        System.out.println("Список содержит Зелёный и Красный: " + colors.containsAll(greenAndRed));
+        // добавление элемента по индексу
+        list.add(2, 22L);  // 2L сдвигается вправо, и на его месте появится 22L
+        System.out.println(list); // список стал таким - [0, 1, 22, 2]
 
-        final Collection<String> yellowOnly = new ArrayList<>();
-        yellowOnly.add("Жёлтый");
-        colors.removeAll(yellowOnly);
+        // вставка элемента в конец списка
+        list.add(4, 4L);
+        System.out.println(list); // обновлённый список - [0, 1, 22, 2, 4]
 
-        System.out.println(colors);
+        // удаление элемента
+        list.remove(4); // по индексу
+        list.remove(2L); //  по значению
+        System.out.println(list); // получился список - [0, 1, 22]
+
+        // получение и вывод элемента по индексу
+        System.out.println(list.get(2)); // напечатали 22
+
+        // замена элемента по индексу
+        list.set(2, 33L);
+        System.out.println(list); // последняя версия списка - [0, 1, 33]
+
     }
 }
