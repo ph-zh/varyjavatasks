634f85fd01ab03b3ef22dff3d755364a8405b5b0
phzh <p.zhavoronkov@gmail.com>
2025-11-03 17:37:18 +0100

    doublyLinkedList example

--- src/HandMadeArrayList.java
+++ src/HandMadeArrayList.java
@@ -1,62 +1,22 @@
 /*
-Попробуйте самостоятельно реализовать ArrayList<T>. Часть класса HandMadeArrayList уже написана. Вам нужно дописать реализацию двух методов.
-Так, grow() должен отвечать за увеличение вместительности массива на 50%, а add(T) добавлять очередной элемент в список.
+
 */
 
-public class HandMadeArrayList<T> {
-
+public class DoublyLinkedList<T> {
+    public Node<T> head;
+    public Node<T> tail;
     private int size = 0;
-    private Object[] elements;
+}
 
-    public HandMadeArrayList() {
-        this.elements = new Object[10];
-    }
+class Node <T> {
 
-    /**
-     * Сначала проверяем, достиг ли размер массива вместимости.
-     * Если достиг — увеличиваем вместимость, иначе сразу добавляем элемент
-     */
-    public void add(T newElement) {
-        // Допишите проверку
-        if (size == elements.length) {
-            grow();
-        }
+    public T data;
+    public Node<T> next;
+    public Node<T> prev;
 
-        /* Допишите код, который добавит очередной элемент в массив
-           и увеличит размер массива на единицу.*/
-        elements[size] = newElement;
-        size++;
-    }
-
-    public T get(int index) {
-        if (index < 0 || index >= size) {
-            throw new ArrayIndexOutOfBoundsException("Element with index " + index + " does not exist");
-        }
-        return (T) this.elements[index];
-    }
-
-    /**
-     * Заменяем текущий массив элементов elements на новый с вместимостью +50%
-     */
-    private void grow() {
-        // Новый массив
-        Object[] newArray = new Object[elements.length + elements.length / 2];
-
-        // Допишите цикл, который копирует все элементы из массива elements в новый массив newArray
-        // Копируем элементы из старого массива в новый
-        for (int i = 0; i < elements.length; i++) {
-            newArray[i] = elements[i];
-        }
-
-        // Возвращаемый новый массив
-        this.elements = newArray;
-    }
-
-    public static void main(String[] args) {
-        final var ar = new HandMadeArrayList<Integer>();
-        for (int i = 0; i < 2000; i++) {
-            ar.add(i);
-        }
-        System.out.println(ar.size);
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
     }
 }
