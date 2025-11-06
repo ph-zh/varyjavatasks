af679087d876a61b479249ffac5c8989d0933b8e
ph.zh <p.zhavoronkov@gmail.com>
2025-11-06 15:46:08 +0100

    collections sorting

--- src/Practicum.java
+++ src/Practicum.java
@@ -4,27 +4,19 @@
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
-public class Practicum {
-
+public class PracticumA {
     public static void main(String[] args) {
-        List<Long> listOfSmallLongs = new ArrayList<>();
-        listOfSmallLongs.add(1L);
-        listOfSmallLongs.add(2L);
-        listOfSmallLongs.add(3L);
+        ArrayList<String> fruits = new ArrayList<>();
+        fruits.add("Банан");
+        fruits.add("Апельсин");
+        fruits.add("Яблоко");
+        fruits.add("Груша");
 
-        List<Long> listOfBigLongs = new ArrayList<>();
-        listOfBigLongs.add(10000000000L);
-        listOfBigLongs.add(20000000000L);
-        listOfBigLongs.add(30000000000L);
+        System.out.println(fruits);
 
-        System.out.println(listOfSmallLongs);
-        System.out.println(listOfBigLongs);
+        Collections.sort(fruits, String.CASE_INSENSITIVE_ORDER);
 
-        Collections.copy(listOfSmallLongs, listOfBigLongs);
-
-        System.out.println(listOfSmallLongs);
-        System.out.println(listOfBigLongs);
+        System.out.println(fruits);
     }
 }
