c01f3e8c17aaa5bb3ff219097a52f17f02a3af54
ph.zh <p.zhavoronkov@gmail.com>
2025-11-06 15:38:06 +0100

    collections overwriting

--- src/Practicum.java
+++ src/Practicum.java
@@ -2,18 +2,21 @@
 
 */
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Practicum {
 
     public static void main(String[] args) {
-        final List<String> emptyList = Collections.emptyList();
-        final List<String> singletonList = Collections.singletonList("Привет, мир!");
-        final List<String> nCopiesList = Collections.nCopies(5, "Java");
+        List<Long> listOfLongs = new ArrayList<>();
+        listOfLongs.add(157478403L);
+        listOfLongs.add(2450033L);
+        listOfLongs.add(32039458858604L);
 
-        System.out.println(emptyList);
-        System.out.println(singletonList);
-        System.out.println(nCopiesList);
+        System.out.println(listOfLongs);
+
+        Collections.fill(listOfLongs, 5555555L);
+        System.out.println(listOfLongs);
     }
 }
