9656fdbea585295963682944466e68b58bd735c1
ph.zh <p.zhavoronkov@gmail.com>
2025-11-06 15:23:24 +0100

    collections example

--- src/DateTimeComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class DateTimeComparator implements Comparator<DateTime> {
-
-    @Override
-    public int compare(DateTime dt1, DateTime dt2) {
-        return Integer.compare(dt1.year, dt2.year);
-    }
-}
--- src/Practicum.java
+++ src/Practicum.java
@@ -1,30 +1,19 @@
 /*
-В приложении по прокату фильмов обнаружилась уязвимость! Для класса, представляющего дату проката фильма DateTime,
-неправильно реализован интерфейс Comparator<T>. Из-за этого любой зритель может брать фильм в прокат на целый год.
-Измените имплементацию интерфейса Comparator<DateTime> — DateTimeComparator таким образом, чтобы она сравнивала
-две даты по всем полям, а не только по году. Взятый в прокат фильм представлен классом RentedFilm.
-Запустите код класса Practicum, чтобы проверить корректность вашей реализации компаратора.
+
 */
+
+import java.util.Collections;
+import java.util.List;
 
 public class Practicum {
 
     public static void main(String[] args) {
-        RentedFilm film1 = new RentedFilm(
-                "Терминатор",
-                new DateTime(20, 11, 2021, 10, 0, 0),
-                new DateTime(27, 11, 2021, 23, 58, 58)
-        );
-        System.out.println("Фильм Терминатор взят в аренду: " + film1.getTimeOfRent());
-        System.out.println("Фильм должен быть вернут до: " + film1.getTimeOfReturn());
+        final List<String> emptyList = Collections.emptyList();
+        final List<String> singletonList = Collections.singletonList("Привет, мир!");
+        final List<String> nCopiesList = Collections.nCopies(5, "Java");
 
-
-        DateTime today = new DateTime(27, 11, 2021, 23, 58, 59);
-
-        System.out.println("Сегодняшнее число: " + today);
-
-        DateTimeComparator comparator = new DateTimeComparator();
-        boolean shouldAlreadyBeReturned = comparator.compare(today, film1.getTimeOfReturn()) > 0;
-
-        System.out.println("Прошло ли время возврата? " + (shouldAlreadyBeReturned ? "Да!" : "Нет!"));
+        System.out.println(emptyList);
+        System.out.println(singletonList);
+        System.out.println(nCopiesList);
     }
 }
