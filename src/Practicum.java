0dfb84a9f0ca126ae0e9304f0c51a0cd479dacf8
phzh <p.zhavoronkov@gmail.com>
2025-11-22 18:15:26 +0100

    HashSet with unique names

--- src/Practicum.java
+++ src/Practicum.java
@@ -1,27 +1,27 @@
 /*
 
 */
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Practicum {
-    // здесь хранятся номера всех выпущенных карт
-    private static Set<String> issuedCards = new HashSet<>();
-
     public static void main(String[] args) {
-        issuedCards.add("3688 2836 2367 0847");
-        issuedCards.add("9522 4377 2788 9805");
-        issuedCards.add("1278 6724 0988 4714");
-        issuedCards.add("9142 7356 9815 9066");
-        issuedCards.add("1168 9734 4967 2984");
+        List<String> allNames = new ArrayList<>();
+        allNames.add("Марья");
+        allNames.add("Пётр");
+        allNames.add("Светлана");
+        allNames.add("Кристина");
+        allNames.add("Иван");
+        allNames.add("Макс");
+        allNames.add("Светлана");
+        allNames.add("Иван");
 
-        String newCard = generateNewCard();
+        Set<String> uniqueNames = new HashSet<>();
+        uniqueNames.addAll(allNames);
 
-        // проверим, свободен ли этот номер карты
-        System.out.println("Номер карты " + newCard + " свободен? " + !issuedCards.contains(newCard));
-    }
-
-    public static String generateNewCard() {
-        return "9734 3477 3844 3702";
+        System.out.println("Количество имён в списке allNames: " + allNames.size());
+        System.out.println("Количество имён в множестве uniqueNames: " + uniqueNames.size());
     }
 }
