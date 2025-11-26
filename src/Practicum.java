03c982128a86a575b3649a5dc46d1d272ef95a67
phzh <p.zhavoronkov@gmail.com>
2025-11-26 20:12:35 +0100

    String uppder and lower cases example

---
+++ src/Practicum.java
@@ -0,0 +1,10 @@
+/*
+
+*/
+public class Practicum {
+    public static void main(String[] args) {
+        String text = "Привет, я Java!";
+        System.out.println(text.toUpperCase()); // выведет "ПРИВЕТ, Я JAVA!"
+        System.out.println(text.toLowerCase()); // выведет "привет, я java!"
+    }
+}
