bfa0f4dfdd52ea3379a5a5e44e321f48f8eaf965
phzh <p.zhavoronkov@gmail.com>
2025-11-26 20:10:09 +0100

    String trim method example

--- src/PasswordChecker.java
+++ src/PasswordChecker.java
@@ -1,25 +1,15 @@
 /*
 
 */
-public class PasswordChecker {
-    public boolean checkPassword(String password) {
-        if (password.isEmpty()) { // проверяем, не является ли строка пустой
-            System.out.println("Пароль отсутствует.");
-            return false;
-        } else if (password.length() < 8) { // размер строки не должен быть меньше 8 символов
-            System.out.println("Пароль слишком короткий.");
-            return false;
-        }
-        System.out.println("Подходящий пароль.");
-        return true;
-    }
-
+public class Practicum {
     public static void main(String[] args) {
-        PasswordChecker checker = new PasswordChecker();
+        String blank = "   ";
+        String notBlank = "Тут пробелы или строка?!";
+        System.out.println(blank.isBlank()); // true
+        System.out.println(notBlank.isBlank()); // false
 
-        System.out.println(checker.checkPassword(""));           // false
-        System.out.println(checker.checkPassword("S34fd1265Jd")); // true
-        System.out.println(checker.checkPassword("          "));  // true
-        System.out.println(checker.checkPassword("shortpw"));    // false
+        String withBlanks = "  Привет, у меня тут лишние пробелы!";
+        String trimmed = withBlanks.trim();
+        System.out.println(trimmed);
     }
 }
