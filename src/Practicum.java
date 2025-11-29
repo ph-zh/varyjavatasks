6a3ed9ae02781ff02ad3c071a9acace7bce0a7d5
phzh <p.zhavoronkov@gmail.com>
2025-11-29 15:00:33 +0100

    String indexOf example

---
+++ src/Practicum.java
@@ -0,0 +1,9 @@
+/*
+
+*/
+public class Practicum {
+    public static void main(String[] args) {
+        String start = "Hello, world!";
+        System.out.println(start.indexOf("world"));
+    }
+}
