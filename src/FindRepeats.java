cb9e93c5eaab68fa39321413cbf20410467a0902
ph.zh <p.zhavoronkov@gmail.com>
2025-12-01 11:53:28 +0100

    numberOfRepeats method not fixed

--- src/FindRepeats.java
+++ src/FindRepeats.java
@@ -1,5 +1,11 @@
 public class FindRepeats {
     int numberOfRepeats(String text, String substring) {
+        int index = 0;
 
+        while(text.contains(substring)) {
+            index = text.indexOf(substring) + substring.length();
+            break;
+        }
+        return index;
     }
 }
