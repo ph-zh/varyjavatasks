d7a4278a5a2ba26f5b683953c0e0b8691b3b7b4d
ph.zh <p.zhavoronkov@gmail.com>
2025-12-02 12:45:54 +0100

    capitalize method updated

--- src/Capitalizator.java
+++ src/Capitalizator.java
@@ -1,5 +1,6 @@
 public class Capitalizator {
     public String capitalize(String str) {
-
+        char firstLetter = str.toUpperCase().charAt(0);
+        return firstLetter + str.substring(1);
     }
 }
