3697ebc2df7045e4387ff0d52a0ebb477fbb6284
ph.zh <p.zhavoronkov@gmail.com>
2025-12-02 13:12:45 +0100

    String replace and replaceFirst methods example

--- src/Capitalizator.java
+++
@@ -1,6 +0,0 @@
-public class Capitalizator {
-    public String capitalize(String str) {
-        char firstLetter = str.toUpperCase().charAt(0);
-        return firstLetter + str.substring(1);
-    }
-}
