388a8ae2388ddb98dad938c96eb6e5a42dddf476
phzh <p.zhavoronkov@gmail.com>
2025-12-07 13:58:51 +0100

    StringBuilder toString example

--- src/Practicum.java
+++ src/Practicum.java
@@ -3,7 +3,13 @@
 */
 public class Practicum {
     public static void main(String[] args) {
-        StringBuilder example = new StringBuilder("Hello, world!");
-        System.out.println(example); // создали строку с начальным значением "Hello, world!"
+        StringBuilder builder = new StringBuilder(); // создали объект - изменяемую строку
+        builder.append("Hello, ");
+        builder.append("world!");
+
+        String asString = builder.toString(); // сделали изменяемую строку неизменяемой
+        builder.append("Frank");
+        builder.append("Spignetti");
+        System.out.println(asString); // выведет "Hello, world!"
     }
 }
