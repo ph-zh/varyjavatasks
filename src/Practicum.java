9aff0216ebc1007f9c13e3f7f3e9de81d9d42d80
phzh <p.zhavoronkov@gmail.com>
2025-12-07 13:31:18 +0100

    StringBuilder example

---
+++ src/Practicum.java
@@ -0,0 +1,9 @@
+/*
+
+*/
+public class Practicum {
+    public static void main(String[] args) {
+        StringBuilder example = new StringBuilder("Hello, world!");
+        System.out.println(example); // создали строку с начальным значением "Hello, world!"
+    }
+}
