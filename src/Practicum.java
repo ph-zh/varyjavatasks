34c6fed01f14f8a80a1fc52a4ead7f741120751d
phzh <p.zhavoronkov@gmail.com>
2025-12-07 15:46:35 +0100

    StringBuilder PoemFixer

--- src/Practicum.java
+++ src/Practicum.java
@@ -1,10 +1,46 @@
 /*
+Часто при реализации больших программ часть данных в них попадает в неподходящем, «сыром» виде.
+Такие данные нужно преобразовать, чтобы избежать ошибок в их дальнейшей обработке. Напишите метод String fixPoem(String[] poem) для работы со стихотворениями.
+Метод должен удалять пустые строки (или строки из пробельных символов) и лишние пробелы с начала и конца. Стихотворение доступно по вызову метода readPoem() в виде списка строк.
+К примеру, если на входе у нас такой набор строк:
 
+" Я помню чудное мгновенье:",
+"   Передо мной явилась ты,   ",
+"",
+"   ",
+" Как мимолётное виденье,",
+" ",
+"Как гений чистой красоты.",
+""
+То после обработки он должен выглядеть так:
+
+"Я помню чудное мгновенье:",
+"Передо мной явилась ты,",
+"Как мимолётное виденье,",
+"Как гений чистой красоты."
 */
-public class Practicum {
+public class PoemFixer {
+
+    public String[] readPoem() {
+        return new String[]{
+                "   Это кто там ложку «ложит»?",
+                "",
+                "   ",
+                "Знай, такого быть не может!",
+                "  Ложку мы на стол кладём,  ",
+                "",
+                "А тебя – к обеду ждём."
+        };
+    }
+
+    public String fixPoem(String[] poem) {
+        ... // допишите код
+    }
+
     public static void main(String[] args) {
-        StringBuilder builder = new StringBuilder("hellodghkwennalmc/ skm");
-        builder.setLength(5); // останется только пять символов
-        System.out.println(builder.toString()); // выведет строку "hello"
+        PoemFixer poemFixer = new PoemFixer();
+        String[] poem = poemFixer.readPoem();
+        String poemAsString = poemFixer.fixPoem(poem);
+        System.out.println(poemAsString);
     }
 }
