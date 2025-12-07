da5a9d3236c29fc9482c3da5b2f6378630a78249
phzh <p.zhavoronkov@gmail.com>
2025-12-07 14:27:51 +0100

    StringBuilder setLngth example

--- src/Practicum.java
+++ src/Practicum.java
@@ -1,21 +1,10 @@
 /*
-🦉 Напишите конструктор приветствия при помощи StringBuilder! Сначала создайте экземпляр StringBuilder, а затем добавьте в него три подстроки.
+
 */
 public class Practicum {
     public static void main(String[] args) {
-
-        String start = "Привет! Меня зовут ";
-
-        StringBuilder hello = new StringBuilder(start); // создайте StringBuilder с началом start
-
-        // добавьте подстроку "<ваше имя>"
-        // добавьте подстроку ". Я из города "
-        // добавьте подстроку "<ваш город>."
-        hello.append("<ваше имя>");
-        hello.append(". Я из города ");
-        hello.append("<ваш город>.");
-
-        String asString = hello.toString();
-        System.out.println(asString);
+        StringBuilder builder = new StringBuilder("hellodghkwennalmc/ skm");
+        builder.setLength(5); // останется только пять символов
+        System.out.println(builder.toString()); // выведет строку "hello"
     }
 }
