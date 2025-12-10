a6de14989b13eb2d58dc94c5a6a6e1d163a1ec21
ph.zh <p.zhavoronkov@gmail.com>
2025-12-10 17:49:12 +0100

    FindRepeats tempalte

---
+++ src/FindRepeats.java
@@ -0,0 +1,26 @@
+/*
+В уроке мы не раз сделали акцент на том, что StringBuilder позволяет не создавать лишние строки.
+Проверьте это! Оптимизируйте программу по подсчету количества вхождений строки в текст из предыдущего урока,
+чтобы она не создавала лишний «мусор». Подсказка: вместо contains() в StringBuilder необходимо пользоваться
+методом indexOf, а вместо получения подстроки можно просто удалять лишние элементы.
+
+        int count = 0;
+        while (text.contains(substring)) {
+            count++;
+            text = text.substring(text.indexOf(substring) + substring.length());
+        }
+        return count;
+        text = text.substring(text.indexOf(entry) + entry.length());
+*/
+public class FindRepeats {
+    int numberOfRepeats(String text, String substring) {
+
+        int count = 0;
+        StringBuilder builder = new StringBuilder(text);
+        while (text.equals(substring)) {
+            text = text.substring(text.indexOf(substring) + substring.length());
+        }
+
+        return count;
+    }
+}
