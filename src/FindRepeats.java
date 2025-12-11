2f1a843a5fe8a0da4067961666aecdfb37276401
phzh <p.zhavoronkov@gmail.com>
2025-12-11 21:47:32 +0100

    FindRepeats task

--- src/FindRepeats.java
+++ src/FindRepeats.java
@@ -4,23 +4,29 @@
 чтобы она не создавала лишний «мусор». Подсказка: вместо contains() в StringBuilder необходимо пользоваться
 методом indexOf, а вместо получения подстроки можно просто удалять лишние элементы.
 
+public class FindRepeats {
+    int numberOfRepeats(String text, String substring) {
         int count = 0;
         while (text.contains(substring)) {
             count++;
             text = text.substring(text.indexOf(substring) + substring.length());
         }
         return count;
-        text = text.substring(text.indexOf(entry) + entry.length());
+    }
+}
 */
 public class FindRepeats {
     int numberOfRepeats(String text, String substring) {
-
         int count = 0;
-        StringBuilder builder = new StringBuilder(text);
-        while (text.equals(substring)) {
-            text = text.substring(text.indexOf(substring) + substring.length());
-        }
 
+        StringBuilder builder = new StringBuilder(text);
+
+        while (builder.indexOf(substring) != -1) {
+            count++;
+            for (int i = 0; i < substring.length(); i++) {
+                builder.deleteCharAt(i);
+            }
+        }
         return count;
     }
 }
