146b88aba18ce097bf1c63eebc341dfe97de2999
ph.zh <p.zhavoronkov@gmail.com>
2025-12-16 21:41:52 +0100

    findRepeats template

--- src/FindRepeats.java
+++ src/FindRepeats.java
@@ -7,6 +7,9 @@
 public class FindRepeats {
     int numberOfRepeats(String text, String substring) {
         int count = 0;
+
+        
+
         while (text.contains(substring)) {
             count++;
             text = text.substring(text.indexOf(substring) + substring.length());
