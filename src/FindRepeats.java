6982ace421f988dd02059a8c3a6ef294736c14e4
ph.zh <p.zhavoronkov@gmail.com>
2025-12-17 21:19:18 +0100

    FindRepeats template

--- src/FindRepeats.java
+++ src/FindRepeats.java
@@ -3,16 +3,22 @@
 Проверьте это! Оптимизируйте программу по подсчету количества вхождений строки в текст из предыдущего урока,
 чтобы она не создавала лишний «мусор». Подсказка: вместо contains() в StringBuilder необходимо пользоваться
 методом indexOf, а вместо получения подстроки можно просто удалять лишние элементы.
+
+Создайте объект StringBuilder через конструктор, принимающий аргумент text.
+После этого замените методы по обработке строки. Вместо text.contains(substring) нужно искать индекс substring в StringBuilder.
+Если он не -1, значит, мы нашли совпадение.
+Если совпадение найдено, нужно отрезать все символы от начала StringBuilder до конца найденной строки.
+Для этого используйте функцию sb.delete(0, ..,). Второй аргумент должен быть индексом следующего за повторением элемента.
 */
 public class FindRepeats {
     int numberOfRepeats(String text, String substring) {
         int count = 0;
 
-        
+        StringBuilder builder = new StringBuilder(text);
 
-        while (text.contains(substring)) {
+        while (builder.indexOf(substring) != -1) {
             count++;
-            text = text.substring(text.indexOf(substring) + substring.length());
+            builder.delete(0, text.indexOf(substring) + substring.length());
         }
         return count;
     }
