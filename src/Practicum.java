5c92e9a89b129159309a544e1855cc151f28e5fc
ph.zh <p.zhavoronkov@gmail.com>
2025-12-25 19:11:17 +0100

    String split method

--- src/Practicum.java
+++ src/Practicum.java
@@ -3,14 +3,12 @@
 */
 public class Practicum {
     public static void main(String[] args) {
-        String excellent = "Марина Голубева,Анна Иванова,Василий Рябов,Екатерина Белых,Иван Засонин,";
+        String excellent = "Марина Голубева,Анна Иванова,Василий Рябов,Екатерина Белых,Иван Засонин";
 
-        int lastNameStart = 0;
-        for (int i = 0; i < excellent.length(); i++) {
-            if (excellent.charAt(i) == ',') {
-                System.out.println(excellent.substring(lastNameStart, i) + " — отлично");
-                lastNameStart = i + 1;
-            }
+        String[] split = excellent.split(",");
+
+        for (String student: split) {
+            System.out.println(student + " — отлично");
         }
     }
 }
