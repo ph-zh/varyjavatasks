dc04ca2142b013d86c595695d415e11907536c0d
ph.zh <p.zhavoronkov@gmail.com>
2026-01-26 09:58:44 +0100

    String of array formatting through printf example

---
+++ src/Practicum.java
@@ -0,0 +1,9 @@
+/*
+
+*/
+public class Practicum {
+    public static void main(String[] args) {
+        String[] colors = new String[]{"красный", "жёлтый", "зелёный"};
+        System.out.printf("Цвета светофора: %s, %s и %s.", colors[0], colors[1], colors[2]);
+    }
+}
