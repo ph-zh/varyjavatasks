0492cf7ab78824a82fd495d2368c98ded6060227
ph.zh <p.zhavoronkov@gmail.com>
2026-02-11 13:31:41 +0100

    HashMap & TreeMap exceptions task template

--- src/Practicum.java
+++ src/Practicum.java
@@ -1,20 +1,27 @@
 /*
-🦉 Добавьте обработку исключений при преобразовании строки в URL в метод parseStringToURI(). При ошибке возвращайте значение null.
+🦉 Даже такая простая операция, как добавление нового элемента в хеш-таблицу может сгенерировать исключение.
+Чтобы было понятно, что пошло не так — добавьте обработку конкретных исключений.
+- Для NullPointerException выведите текст — «Реализация не поддерживает неинициализированные объекты.».
+- Для UnsupportedOperationException — сообщение «Реализация не поддерживает добавление новых объектов.».
 */
-import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Practicum {
     public static void main(String[] args) {
-        System.out.println(parseStringToURI("https://ya.ru"));
-        System.out.println(parseStringToURI("\\\\"));
-        System.out.println(parseStringToURI(null));
+        System.out.println(addToMap(new HashMap<>(), "test", "test"));
+        System.out.println(addToMap(new HashMap<>(), null, null));
+
+        System.out.println(addToMap(new TreeMap<>(), "test", "test"));
+        System.out.println(addToMap(new TreeMap<>(), null, null));
+
+        System.out.println(addToMap(Map.of(), "test", "test"));
     }
 
-    public static URI parseStringToURI(final String input) {
-        try {
-            return new URI(input);
-        } catch (Throwable exception) {
-            return null;
-        }
+    public static Map<String, Object> addToMap(final Map<String, Object> storage,
+                                               final String key, final Object value) {
+        storage.put(key, value);
+        return storage;
     }
 }
